87312abddf0cd0b3e61d7100583514a2e77d3ae3
bear101 <contact@bearware.dk>
2014-07-11 17:22:35 +0200

    Header updated to 5.0.0.3201

--- TeamTalk_DLL/TeamTalk.h
+++ TeamTalk_DLL/TeamTalk.h
@@ -16,7 +16,7 @@
  * client's version can be seen in the @a szVersion member of the
  * #User-struct. */
 
-#define TEAMTALK_VERSION "5.0.0.3178"
+#define TEAMTALK_VERSION "5.0.0.3201"
 
 
 #if defined(WIN32)
