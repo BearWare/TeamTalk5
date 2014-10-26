4b672f1daca4b206990d973e5a7f2c6ebf70cbac
bdr <bdr@raspberrypi.(none)>
2014-10-26 17:05:08 +0100

    Updated API to version 5.0.0.3529. Introduced SpeexDSP which replaces most settings from AudioConfig. SetUserAudioFolder() replaced by SetUserMediaStorageDir() AudioCodec's nMSecPerPacket renamed to nTxIntervalMSec. AudioBlocks are now requested basec on user and stream type. .NET API now mainly uses 'ref' instead of 'out'. Removed GetBannedUsers() and GetUserAccounts(). These are now retrieved through events.

---
+++ TeamTalkJNI/src/dk/bearware/SpeexDSP.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2005-2014, BearWare.dk
+ * 
+ * Contact Information:
+ *
+ * Bjoern D. Rasmussen
+ * Skanderborgvej 40 4-2
+ * DK-8000 Aarhus C
+ * Denmark
+ * Email: contact@bearware.dk
+ * Phone: +45 20 20 54 59
+ * Web: http://www.bearware.dk
+ *
+ * This source code is part of the TeamTalk 5 SDK owned by
+ * BearWare.dk. All copyright statements may not be removed 
+ * or altered from any source distribution. If you use this
+ * software in a product, an acknowledgment in the product 
+ * documentation is required.
+ *
+ */
+
+package dk.bearware;
+
+public class SpeexDSP
+{
+    public boolean bEnableAGC;
+    public int nGainLevel;
+    public int nMaxIncDBSec;
+    public int nMaxDecDBSec;
+    public int nMaxGainDB;
+    public boolean bEnableDenoise;
+    public int nMaxNoiseSuppressDB;
+    public boolean bEnableEchoCancellation;
+    public int nEchoSuppress;
+    public int nEchoSuppressActive;
+
+    public SpeexDSP() {
+    }
+
+    public SpeexDSP(boolean set_defaults) {
+        if(!set_defaults)
+            return;
+
+        bEnableAGC = SpeexDSPConstants.DEFAULT_AGC_ENABLE;
+        nGainLevel = SpeexDSPConstants.DEFAULT_AGC_GAINLEVEL;
+        nMaxIncDBSec = SpeexDSPConstants.DEFAULT_AGC_INC_MAXDB;
+        nMaxDecDBSec = SpeexDSPConstants.DEFAULT_AGC_DEC_MAXDB;
+        nMaxGainDB = SpeexDSPConstants.DEFAULT_AGC_GAINMAXDB;
+        bEnableDenoise = SpeexDSPConstants.DEFAULT_DENOISE_ENABLE;
+        nMaxNoiseSuppressDB = SpeexDSPConstants.DEFAULT_DENOISE_SUPPRESS;
+        bEnableEchoCancellation = SpeexDSPConstants.DEFAULT_ECHO_ENABLE;
+        nEchoSuppress = SpeexDSPConstants.DEFAULT_ECHO_SUPPRESS;
+        nEchoSuppressActive = SpeexDSPConstants.DEFAULT_ECHO_SUPPRESS_ACTIVE;
+    }
+}
---
+++ TeamTalkJNI/src/dk/bearware/SpeexDSPConstants.java
@@ -0,0 +1,15 @@
+package dk.bearware;
+
+public interface SpeexDSPConstants
+{
+    public static final boolean DEFAULT_AGC_ENABLE = true;
+    public static final int DEFAULT_AGC_GAINLEVEL = 8000;
+    public static final int DEFAULT_AGC_INC_MAXDB = 12;
+    public static final int DEFAULT_AGC_DEC_MAXDB = -40;
+    public static final int DEFAULT_AGC_GAINMAXDB = 30;
+    public static final boolean DEFAULT_DENOISE_ENABLE = true;
+    public static final int DEFAULT_DENOISE_SUPPRESS = -30;
+    public static final boolean DEFAULT_ECHO_ENABLE = true;
+    public static final int DEFAULT_ECHO_SUPPRESS = -40;
+    public static final int DEFAULT_ECHO_SUPPRESS_ACTIVE = -15;
+}
