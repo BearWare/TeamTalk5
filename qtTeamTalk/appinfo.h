7f180d0057a86e4f0ba2ca4d82fabafed6e8c025
bear101 <contact@bearware.dk>
2014-12-11 21:29:43 +0100

    Version update, 5.0BETA2

--- qtTeamTalk/appinfo.h
+++ qtTeamTalk/appinfo.h
@@ -23,7 +23,7 @@
 #define APPINFO_H
 
 #define COMPANYNAME         "BearWare.dk"
-#define APPVERSION          "5.0 - BETA1"
+#define APPVERSION          "5.0 - BETA2"
 #define APPVERSION_SHORT    "5.0"
 #define APPICON             QString::fromUtf8(":/images/images/teamtalk.png")
 #define APPTRAYICON         QString::fromUtf8(":/images/images/teamtalky.png")
