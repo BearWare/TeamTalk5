6293fa7698c056313c81ca911453c30c45705be8
bear101 <contact@bearware.dk>
2015-10-02 22:38:29 +0200

    TeamTalk 5.1b SDK

--- TeamTalk_DLL/TeamTalk.h
+++ TeamTalk_DLL/TeamTalk.h
@@ -16,7 +16,7 @@
  * client's version can be seen in the @a szVersion member of the
  * #User-struct. */
 
-#define TEAMTALK_VERSION "5.1.1.4315"
+#define TEAMTALK_VERSION "5.1.1.4334"
 
 
 #if defined(WIN32)
