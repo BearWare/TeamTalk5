926212f83f5bb2b74f86f9b7c5d4814edbabc71c
bear101 <contact@bearware.dk>
2016-02-25 22:01:31 +0100

    ttserver log examples

---
+++ Examples/ttserverlog.net/Program.cs
@@ -0,0 +1,355 @@
+/*
+ * Copyright (c) 2005-2016, BearWare.dk
+ * 
+ * Contact Information:
+ *
+ * Bjoern D. Rasmussen
+ * Skanderborgvej 40 4-2
+ * DK-8000 Aarhus C
+ * Denmark
+ * Email: contact@bearware.dk
+ * Phone: +45 20 20 54 59
+ * Web: http://www.bearware.dk
+ *
+ * This source code is part of the TeamTalk 5 SDK owned by
+ * BearWare.dk. All copyright statements may not be removed 
+ * or altered from any source distribution. If you use this
+ * software in a product, an acknowledgment in the product 
+ * documentation is required.
+ *
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Diagnostics;
+using BearWare;
+
+namespace ttserverlog.net
+{
+    class Program
+    {
+        static BearWare.TeamTalk ttclient;
+
+        static string audio_dir = "c:\\temp";
+
+        static int cur_cmd_id = 0;
+
+        [STAThread] //this macro must be set in console applications for TeamTalk to work properly
+        static void Main(string[] args)
+        {
+            ttclient = new TeamTalk(true);
+
+            string ipaddr = "192.168.1.110";
+            string username = "admin", password = "admin";
+            int tcpport = TeamTalk.DEFAULT_TCPPORT, udpport = TeamTalk.DEFAULT_UDPPORT;
+            bool encrypted = false;
+
+            Console.WriteLine("TeamTalk 5 server logger.");
+            Console.WriteLine();
+            Console.WriteLine("TeamTalk 5 server login information.");
+            Console.Write("Specify IP-address of server to connect to: ");
+            ipaddr = get_str(ipaddr);
+            Console.Write("Specify TCP port: ");
+            tcpport = get_int(tcpport);
+            Console.Write("Specify UDP port: ");
+            udpport = get_int(udpport);
+            Console.Write("Is server encrypted? ");
+            encrypted = get_str("N").ToLower() == "y";
+            Console.WriteLine("Administrator account information.");
+            Console.Write("Specify username: ");
+            username = get_str(username);
+            Console.Write("Specify password: ");
+            password = get_str(password);
+            Console.Write("Specify directory where to store audio: ");
+            audio_dir = get_str(audio_dir);
+
+            if(!File.Exists("lame_enc.dll"))
+                Console.WriteLine("lame_enc.dll not found, so audio will be stored to .wav instead of .mp3");
+
+            //hook events for checking connection
+            ttclient.OnConnectionFailed += new TeamTalk.Connection(ttclient_OnConnectFailed);
+            ttclient.OnConnectionSuccess += new TeamTalk.Connection(ttclient_OnConnectSuccess);
+            ttclient.OnConnectionLost += new TeamTalk.Connection(ttclient_OnConnectionLost);
+
+            //now that we got all the information we needed we can connect and logon
+            if (!ttclient.Connect(ipaddr, tcpport, udpport, 0, 0, encrypted))
+            {
+                Console.WriteLine("Failed to connect to server");
+                return;
+            }
+
+            TTMessage msg = new TTMessage();
+            //wait for 10 seconds for connect event
+            if (!ttclient.GetMessage(ref msg, 10000))
+            {
+                Console.WriteLine("Did not connect in time. Aborting...");
+                return;
+            }
+
+            Debug.Assert((ttclient.Flags & ClientFlag.CLIENT_CONNECTED) == ClientFlag.CLIENT_CONNECTED);
+
+            //hook the remaining events we want to process
+            ttclient.OnCmdMyselfLoggedIn += new TeamTalk.MyselfLoggedIn(ttclient_OnCmdMyselfLoggedIn);
+            ttclient.OnCmdServerUpdate += new TeamTalk.ServerUpdate(ttclient_OnCmdServerUpdate);
+            ttclient.OnCmdChannelNew += new TeamTalk.ChannelUpdate(ttclient_OnCmdChannelNew);
+            ttclient.OnCmdChannelUpdate += new TeamTalk.ChannelUpdate(ttclient_OnCmdChannelUpdate);
+            ttclient.OnCmdChannelRemove += new TeamTalk.ChannelUpdate(ttclient_OnCmdChannelRemove);
+            ttclient.OnCmdUserLoggedIn += new TeamTalk.UserUpdate(ttclient_OnCmdUserLoggedIn);
+            ttclient.OnCmdUserLoggedOut += new TeamTalk.UserUpdate(ttclient_OnCmdUserLoggedOut);
+            ttclient.OnCmdUserUpdate += new TeamTalk.UserUpdate(ttclient_OnCmdUserUpdate);
+            ttclient.OnCmdUserJoinedChannel += new TeamTalk.UserUpdate(ttclient_OnCmdUserJoinedChannel);
+            ttclient.OnCmdUserLeftChannel += new TeamTalk.UserUpdate(ttclient_OnCmdUserLeftChannel);
+            ttclient.OnCmdFileNew += new TeamTalk.FileUpdate(ttclient_OnCmdFileNew);
+            ttclient.OnCmdFileRemove += new TeamTalk.FileUpdate(ttclient_OnCmdFileRemove);
+            ttclient.OnCmdUserTextMessage += new TeamTalk.UserTextMessage(ttclient_OnCmdUserTextMessage);
+            ttclient.OnCmdProcessing += new TeamTalk.CommandProcessing(ttclient_OnCmdProcessing);
+            ttclient.OnCmdError += new TeamTalk.CommandError(ttclient_OnCmdError);
+            ttclient.OnCmdSuccess += new TeamTalk.CommandSuccess(ttclient_OnCmdSuccess);
+            ttclient.OnUserRecordMediaFile += new TeamTalk.UserRecordMediaFile(ttclient_OnUserAudioFile);
+
+            //now that we're connected log on
+            cur_cmd_id = ttclient.DoLogin("ttserverlog", username, password);
+            Console.WriteLine("Login command got cmd ID #" + cur_cmd_id);
+            if (cur_cmd_id < 0)
+            {
+                Console.WriteLine("Failed to issue command. Aborting...");
+                return;
+            }
+
+            //wait for login command to complete
+            while (ttclient.GetMessage(ref msg, 10000))
+            {
+                ttclient.ProcessMsg(msg);
+                if (msg.nClientEvent == ClientEvent.CLIENTEVENT_CMD_PROCESSING &&
+                    msg.nSource == cur_cmd_id && (bool)msg.DataToObject() /* bActive */ == false)
+                    break;
+            }
+
+            //if this assertion failed it's because the login information was incorrect
+            Debug.Assert((ttclient.Flags & ClientFlag.CLIENT_AUTHORIZED) == ClientFlag.CLIENT_AUTHORIZED);
+            //we have to be admin user to do logging on the server.
+            Debug.Assert((ttclient.UserType & UserType.USERTYPE_ADMIN) == UserType.USERTYPE_ADMIN);
+
+            //now process events forever.
+            while (ttclient.GetMessage(ref msg, -1))
+            {
+                ttclient.ProcessMsg(msg);
+            }
+        }
+
+        static void ttclient_OnUserAudioFile(int nUserID, MediaFileInfo mediafileinfo)
+        {
+            User user = new User();
+            if (ttclient.GetUser(nUserID, ref user))
+            {
+                switch (mediafileinfo.nStatus)
+                {
+                    case MediaFileStatus.MFS_ERROR :
+                        Console.WriteLine("Failed to store audio file for #{0} {1}.",
+                            user.nUserID, user.szNickname);
+                        break;
+                    case MediaFileStatus.MFS_STARTED :
+                        Console.WriteLine("Storing audio from #{0} for {1} to file.",
+                            user.nUserID, user.szNickname);
+                        break;
+                    case MediaFileStatus.MFS_FINISHED :
+                        Console.WriteLine("Finished storing audio from #{0} {1} to file.",
+                            user.nUserID, user.szNickname);
+                        break;
+                    case MediaFileStatus.MFS_ABORTED :
+                        Console.WriteLine("Aborted storing audio from #{0} {1} to file.",
+                            user.nUserID, user.szNickname);
+                        break;
+                }
+            }
+        }
+
+        static void ttclient_OnCmdError(int nCmdID, ClientErrorMsg clienterrormsg)
+        {
+            Console.WriteLine("Command with ID #{0} failed with the following error: {1}",
+                nCmdID, clienterrormsg.szErrorMsg);
+        }
+
+        static void ttclient_OnCmdSuccess(int nCmdID)
+        {
+            Console.WriteLine("Command with ID #{0} succeeded.", nCmdID);
+        }
+
+        static void ttclient_OnCmdProcessing(int nCmdID, bool bActive)
+        {
+            if (bActive)
+                Console.WriteLine("Started processing cmd ID #{0}", nCmdID);
+            else
+            {
+                Console.WriteLine("Finished processing cmd ID #{0}", nCmdID);
+                cur_cmd_id = 0;
+            }
+        }
+
+        static void ttclient_OnCmdUserTextMessage(TextMessage textmessage)
+        {
+            User user = new User(), touser = new User();
+            if (!ttclient.GetUser(textmessage.nFromUserID, ref user))
+                return;
+            ttclient.GetUser(textmessage.nToUserID, ref touser);
+            string chanpath = "";
+            ttclient.GetChannelPath(textmessage.nChannelID, ref chanpath);
+            switch (textmessage.nMsgType)
+            {
+                case TextMsgType.MSGTYPE_USER :
+                    Console.WriteLine("Text message from user #{0} {1} to user #{2} {3} content: {4}",
+                        user.nUserID, user.szNickname, touser.nUserID, touser.szNickname, textmessage.szMessage);
+                    break;
+                case TextMsgType.MSGTYPE_CHANNEL :
+                    Console.WriteLine("Text message from user #{0} {1} to channel {2} context: {3}",
+                        user.nUserID, user.szNickname, chanpath, textmessage.szMessage);
+                    break;
+                case TextMsgType.MSGTYPE_BROADCAST :
+                    Console.WriteLine("Text messagr from user #{0} {1} to entire server content: {2}",
+                        user.nUserID, user.szNickname, textmessage.szMessage);
+                    break;
+            }
+        }
+
+        static void ttclient_OnCmdFileRemove(RemoteFile fileinfo)
+        {
+            string chanpath = "";
+            if (ttclient.GetChannelPath(fileinfo.nChannelID, ref chanpath))
+                Console.WriteLine("File #{0} {1} removed from channel {1}", 
+                    fileinfo.nFileID, fileinfo.szFileName, chanpath);
+        }
+
+        static void ttclient_OnCmdFileNew(RemoteFile fileinfo)
+        {
+            string chanpath = "";
+            if (ttclient.GetChannelPath(fileinfo.nChannelID, ref chanpath))
+                Console.WriteLine("File #{0} {1} added to channel {2}",
+                    fileinfo.nFileID, fileinfo.szFileName, chanpath);
+
+        }
+
+        static void ttclient_OnCmdUserLeftChannel(User user)
+        {
+            string chanpath = "";
+            if (ttclient.GetChannelPath(user.nChannelID, ref chanpath))
+                Console.WriteLine("User #{0} {1} left {2}",
+                    user.nUserID, user.szNickname, chanpath);
+        }
+
+        static void ttclient_OnCmdUserJoinedChannel(User user)
+        {
+            string chanpath = "";
+            if (ttclient.GetChannelPath(user.nChannelID, ref chanpath))
+            {
+                Console.WriteLine("User #{0} {1} joined {2}",
+                    user.nUserID, user.szNickname, chanpath);
+                int cmd_id = ttclient.DoSubscribe(user.nUserID, Subscription.SUBSCRIBE_INTERCEPT_USER_MSG |
+                    Subscription.SUBSCRIBE_INTERCEPT_CHANNEL_MSG | Subscription.SUBSCRIBE_INTERCEPT_VOICE);
+                if (cmd_id < 0)
+                    Console.WriteLine("Failed to issue subscribe command");
+                else
+                    Console.WriteLine("Issued command to subscribe to #{0}", user.nUserID);
+            }
+        }
+
+        static void ttclient_OnCmdUserUpdate(User user)
+        {
+            string chanpath = "";
+            if (ttclient.GetChannelPath(user.nChannelID, ref chanpath))
+                Console.WriteLine("User #{0} {1} updated {2}",
+                    user.nUserID, user.szNickname, chanpath);
+        }
+
+        static void ttclient_OnCmdUserLoggedOut(User user)
+        {
+            //cannot retrieve user since he no longer exists
+            Console.WriteLine("User #{0} logged out", user.nUserID);
+        }
+
+        static void ttclient_OnCmdUserLoggedIn(User user)
+        {
+            Console.WriteLine("User #{0} {1} logged in",
+                user.nUserID, user.szNickname);
+            ttclient.SetUserMediaStorageDir(user.nUserID, audio_dir, "", AudioFileFormat.AFF_WAVE_FORMAT);
+        }
+
+        static void ttclient_OnCmdChannelRemove(Channel channel)
+        {
+            //cannot retrieve
+            Console.WriteLine("Removed channel #{0}", channel.nChannelID);
+        }
+
+        static void ttclient_OnCmdChannelUpdate(Channel channel)
+        {
+            string chanpath = "";
+            if (ttclient.GetChannelPath(channel.nChannelID, ref chanpath))
+                Console.WriteLine("Updated channel {0}", chanpath);
+        }
+
+        static void ttclient_OnCmdChannelNew(Channel channel)
+        {
+            string chanpath = "";
+            if (ttclient.GetChannelPath(channel.nChannelID, ref chanpath))
+            {
+                Console.WriteLine("Added channel {0}. Audio storage: {1}", chanpath, audio_dir);
+            }
+        }
+
+        static void ttclient_OnCmdServerUpdate(ServerProperties serverproperties)
+        {
+            Console.WriteLine("Get new server properties.");
+            Console.WriteLine("Server Name: " + serverproperties.szServerName);
+            Console.WriteLine("MOTD: " + serverproperties.szMOTD);
+            Console.WriteLine("Server Version: " + serverproperties.szServerVersion);
+        }
+
+        static void ttclient_OnCmdMyselfLoggedIn(int nMyUserID, UserAccount useraccount)
+        {
+            Console.WriteLine("Logged in successfully...");
+            Console.WriteLine("Got user ID #" + nMyUserID);
+            Debug.Assert(nMyUserID == ttclient.UserID);
+        }
+
+        static void ttclient_OnConnectFailed()
+        {
+            Console.WriteLine("Failed to connect to server");
+        }
+
+        static void ttclient_OnConnectSuccess()
+        {
+            Console.WriteLine("Connected successfully to server");
+        }
+
+        static void ttclient_OnConnectionLost()
+        {
+            Console.WriteLine("Connection was lost to the server");
+        }
+
+
+
+
+
+
+
+
+
+
+
+        static string get_str(string def_str)
+        {
+            Console.Write("(\"" + def_str + "\")");
+            string str = Console.ReadLine();
+            if (str.Length == 0)
+                return def_str;
+            return str;
+        }
+
+        static int get_int(int def_int)
+        {
+            string str = get_str(def_int.ToString());
+            return int.Parse(str);
+        }
+    }
+}
---
+++ Examples/ttserverlog/Main.cpp
@@ -0,0 +1,304 @@
+#ifdef WIN32
+#include <windows.h>
+#endif
+#include <sys/stat.h>
+#include <TeamTalk.h>
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <assert.h>
+#include <stdlib.h>
+using namespace std;
+
+TTInstance* ttInst;
+
+#ifdef UNICODE
+#define cout wcout
+#define cin wcin
+#define string wstring
+#define ostringstream wostringstream
+#define istringstream wistringstream
+#define _T(x) L ## x
+#else
+#define _T
+#endif
+
+#define DEFAULT_IPADDR      _T("")
+#define DEFAULT_TCPPORT     10333
+#define DEFAULT_UDPPORT     10333
+#define DEFAULT_USERNAME    _T("")
+#define DEFAULT_PASSWORD    _T("")
+
+#define DEFAULT_AUDIO_STORAGE _T("")
+
+string audio_dir;
+
+void processTTMessage(const TTMessage& msg);
+string get_str(const string& input);
+int get_int(int value);
+
+int main(int argc, TTCHAR* argv[])
+{
+    string ipaddr;
+    string username, password;
+    int tcpport, udpport;
+    bool encrypted = false;
+    TTMessage msg;
+    int wait_ms, cmd_id;
+    struct stat s_file;
+
+    ttInst = TT_InitTeamTalkPoll();
+
+    cout << "TeamTalk 5 server logger." << endl;
+    cout << endl << "TeamTalk 5 server login information." << endl;
+    cout << "Specify IP-address of server to connect to: ";
+    ipaddr = get_str(DEFAULT_IPADDR);
+    cout << "Specify TCP port: ";
+    tcpport = get_int(DEFAULT_TCPPORT);
+    cout << "Specify UDP port: ";
+    udpport = get_int(DEFAULT_UDPPORT);
+    cout << "Is server encrypted? ";
+    encrypted = get_str(_T("n")) == _T("y");
+    cout << endl << "Administrator account information." << endl;
+    cout << "Specify username: ";
+    username = get_str(DEFAULT_USERNAME);
+    cout << "Specify password: ";
+    password = get_str(DEFAULT_PASSWORD);
+    cout << "Specify directory where to store audio: " << endl;
+    audio_dir = get_str(DEFAULT_AUDIO_STORAGE);
+
+    int i = stat ( "lame_enc.dll", &s_file );
+    if ( i != 0 )
+        cout << "lame_enc.dll not found, so audio will be stored to .wav instead of .mp3" << endl;
+
+    //now that we got all the information we needed we can connect and logon
+    if(!TT_Connect(ttInst, ipaddr.c_str(), tcpport, udpport, 0, 0, false))
+        goto error_connect;
+    
+    //wait for connect event
+    wait_ms = 10000;
+    if(!TT_GetMessage(ttInst, &msg, &wait_ms) || 
+        msg.nClientEvent == CLIENTEVENT_CON_FAILED)
+        goto error_connect;
+    assert(msg.nClientEvent == CLIENTEVENT_CON_SUCCESS);
+    assert(TT_GetFlags(ttInst) & CLIENT_CONNECTED);
+    cout << "Now Connected..." << endl;
+
+    //now that we're connected log on
+    cmd_id = TT_DoLogin(ttInst, _T(""), username.c_str(), password.c_str());
+    if(cmd_id < 0)
+        goto error_login;
+
+    cout << "Login command got cmd ID #" << cmd_id << endl;
+
+    //wait for server reply
+    if(!TT_GetMessage(ttInst, &msg, &wait_ms) ||
+        msg.nClientEvent != CLIENTEVENT_CMD_PROCESSING)
+        goto error_login;
+    //get response
+    if(!TT_GetMessage(ttInst, &msg, &wait_ms) ||
+        msg.nClientEvent == CLIENTEVENT_CMD_ERROR)
+        goto error_login;
+
+    //wait for login command to complete
+    //client will now post all the server information
+    while(TT_GetMessage(ttInst, &msg, &wait_ms) &&
+        msg.nClientEvent != CLIENTEVENT_CMD_PROCESSING)
+    {
+        processTTMessage(msg);
+    }
+    assert(msg.bActive == FALSE); //command processing complete
+    assert(TT_GetFlags(ttInst) & CLIENT_AUTHORIZED); //we're authorized
+    //ensure account we used is administrator
+    assert(TT_GetMyUserType(ttInst) & USERTYPE_ADMIN);
+
+    //now just process events forever
+    while(TT_GetMessage(ttInst, &msg, NULL))
+        processTTMessage(msg);
+
+	return EXIT_SUCCESS;
+error_connect:
+    cout << "Failed to connect to server." << endl;
+    return EXIT_FAILURE;
+error_login:
+    cout << "Failed to log on to server." << endl;
+    return EXIT_FAILURE;
+}
+
+void processTTMessage(const TTMessage& msg)
+{
+    switch (msg.nClientEvent)
+    {
+    case CLIENTEVENT_CMD_MYSELF_LOGGEDIN:
+        cout << "Logged in successfully..." << endl;
+        cout << "Got user ID #" << msg.user.nUserID << endl;
+        break;
+    case CLIENTEVENT_CMD_SERVER_UPDATE:
+            cout << "Got new server properties:" << endl;
+            cout << "Server password: " << msg.serverproperties.szServerName << endl;
+            cout << "MOTD: " << msg.serverproperties.szMOTD << endl;
+            cout << "Server version: " << msg.serverproperties.szServerVersion << endl;
+        break;
+    case CLIENTEVENT_CMD_CHANNEL_NEW:
+    {
+        TTCHAR buf[TT_STRLEN];
+        if (!TT_GetChannelPath(ttInst, msg.channel.nChannelID, buf))
+            break;
+        cout << "Added channel " << buf << endl;
+        break;
+    }
+    case CLIENTEVENT_CMD_CHANNEL_UPDATE:
+    {
+        TTCHAR buf[TT_STRLEN];
+        if (!TT_GetChannelPath(ttInst, msg.channel.nChannelID, buf))
+            break;
+        cout << "Updated channel " << buf << endl;
+        break;
+    }
+    case CLIENTEVENT_CMD_CHANNEL_REMOVE:
+        //cannot retrieve channel since it no longer exists.
+        cout << "Removed channel #" << msg.channel.nChannelID << endl;
+        break;
+    case CLIENTEVENT_CMD_USER_LOGGEDIN:
+    {
+        cout << "User #" << msg.user.nUserID << " " << msg.user.szNickname << " logged in" << endl;
+        cout << "Setting audio storage: " << audio_dir << endl;
+        TT_SetUserMediaStorageDir(ttInst, msg.user.nUserID, audio_dir.c_str(), NULL, AFF_WAVE_FORMAT);
+        break;
+    }
+    case CLIENTEVENT_CMD_USER_LOGGEDOUT:
+        cout << "User #" << msg.user.nUserID << " " << msg.user.szNickname << " logged out" << endl;
+        break;
+    case CLIENTEVENT_CMD_USER_UPDATE:
+    {
+        TTCHAR buf[TT_STRLEN];
+        if (TT_GetChannelPath(ttInst, msg.user.nChannelID, buf))
+            cout << "User #" << msg.user.nUserID << " " << msg.user.szNickname << " updated " << buf << endl;
+        break;
+    }
+    case CLIENTEVENT_CMD_USER_JOINED:
+    {
+        TTCHAR buf[TT_STRLEN];
+        if (TT_GetChannelPath(ttInst, msg.user.nChannelID, buf))
+        {
+            cout << "User #" << msg.user.nUserID << " " << msg.user.szNickname << " joined " << buf << endl;
+            int cmd_id = TT_DoSubscribe(ttInst, msg.user.nUserID,
+                SUBSCRIBE_INTERCEPT_USER_MSG | SUBSCRIBE_INTERCEPT_CHANNEL_MSG |
+                SUBSCRIBE_INTERCEPT_VOICE);
+            if (cmd_id > 0)
+                cout << "Subscribing to text and audio events from #" << msg.user.nUserID << endl;
+            else
+                cout << "Failed to issue subscribe command" << endl;
+        }
+        break;
+    }
+    case CLIENTEVENT_CMD_USER_LEFT:
+    {
+        TTCHAR buf[TT_STRLEN];
+        if (TT_GetChannelPath(ttInst, msg.user.nChannelID, buf))
+            cout << "User #" << msg.user.nUserID << " " << msg.user.szNickname << " left " << buf << endl;
+        break;
+    }
+    case CLIENTEVENT_CMD_FILE_NEW:
+    {
+        TTCHAR buf[TT_STRLEN];
+        const RemoteFile& file = msg.remotefile;
+        if (TT_GetChannelPath(ttInst, msg.nSource, buf))
+            cout << "File #" << file.nFileID << " " << file.szFileName << " added to channel " << buf << endl;
+        break;
+    }
+    case CLIENTEVENT_CMD_FILE_REMOVE:
+    {
+        TTCHAR buf[TT_STRLEN];
+        const RemoteFile& file = msg.remotefile;
+        //we can't extract it since it no longer exists
+        if (TT_GetChannelPath(ttInst, msg.nSource, buf))
+            cout << "File #" << file.nFileID << " removed from channel " << buf << endl;
+        break;
+    }
+    case CLIENTEVENT_CMD_USER_TEXTMSG :
+    {
+        TTCHAR buf[TT_STRLEN];
+        User user;
+        const TextMessage& textmsg = msg.textmessage;
+        User touser;
+        if (!TT_GetUser(ttInst, textmsg.nFromUserID, &user))
+            break;
+        TT_GetUser(ttInst, textmsg.nToUserID, &touser);
+        TT_GetChannelPath(ttInst, textmsg.nChannelID, buf);
+        switch(textmsg.nMsgType)
+        {
+        case MSGTYPE_USER :
+            cout << "Text message from user #" << user.nUserID << " ";
+            cout << user.szNickname << " to user #" << touser.nUserID << " ";
+            cout << touser.szNickname << " ";
+            cout << "content: " << textmsg.szMessage << endl;
+            break;
+        case MSGTYPE_CHANNEL :
+            cout << "Text message from user #" << user.nUserID << " ";
+            cout << user.szNickname << " to channel " << buf << " ";
+            cout << "content: " << textmsg.szMessage << endl;
+            break;
+        case MSGTYPE_BROADCAST :
+            cout << "Text message from user #" << user.nUserID << " ";
+            cout << user.szNickname << " to entire server ";
+            cout << "content: " << textmsg.szMessage << endl;
+            break;
+        }
+    }
+    break;
+    case CLIENTEVENT_CMD_PROCESSING :
+        if(msg.bActive == TRUE)
+            cout << "Started processing cmd ID #" << msg.nSource << endl;
+        else
+            cout << "Finished processing cmd ID #" << msg.nSource << endl;
+        break;
+    case CLIENTEVENT_USER_RECORD_MEDIAFILE :
+    {
+        User user;
+        if(TT_GetUser(ttInst, msg.nSource, &user))
+        {
+            switch(msg.mediafileinfo.nStatus)
+            {
+            case MFS_ERROR :
+                cout << "Failed to store audio file from #" << user.nUserID << " ";
+                cout << user.szNickname << endl;
+                break;
+            case MFS_STARTED :
+                cout << "Storing audio from #" << user.nUserID << " ";
+                cout << user.szNickname << " to file." << endl;
+                break;
+            case MFS_FINISHED :
+                cout << "Finished storing audio from #" << user.nUserID << " ";
+                cout << user.szNickname << " to file." << endl;
+                break;
+            case MFS_ABORTED :
+                cout << "Aborted storing audio from #" << user.nUserID << " ";
+                cout << user.szNickname << " to file." << endl;
+                break;
+            }
+        }
+        break;
+    }
+    default :
+        cout << "Unknown message #" << msg.nClientEvent << " nSource " << msg.nSource << " ttType " << msg.ttType << endl;
+    }
+}
+
+string get_str(const string& input)
+{
+    cout << "(\"" << input << "\") ";
+    string tmp;
+    std::getline(cin, tmp);
+    return tmp.empty()? input : tmp;
+}
+
+int get_int(int value)
+{
+    ostringstream os;
+    os << value;
+    string sval = get_str(os.str());
+    istringstream is(sval);
+    is >> value;
+    return value;
+}
