cef71ee933cc42aaf6d93ba8ac56c34d34c3875e
bear101 <contact@bearware.dk>
2017-05-05 09:19:17 +0200

    Updated version info

--- TeamTalkClassic/AppInfo.h
+++ TeamTalkClassic/AppInfo.h
@@ -23,7 +23,7 @@
 
 #define COMPANYNAME             _T("BearWare.dk")
 #define APPVERSION_SHORT        _T("5.2.2")
-#define APPVERSION              _T( TEAMTALK_VERSION ) _T(" - Unreleased")
+#define APPVERSION              _T( "5.2.2" ) _T("")
 
 #ifdef ENABLE_ENCRYPTION
 #define APPTITLE                _T("TeamTalk 5 Classic Professional v. ") APPVERSION
