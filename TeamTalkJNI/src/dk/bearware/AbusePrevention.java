8ac2699530a9267de01d203a3d7228404cecfdc7
bear101 <contact@bearware.dk>
2017-05-23 21:49:02 +0200

    Added missing class

---
+++ TeamTalkJNI/src/dk/bearware/AbusePrevention.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2005-2017, BearWare.dk
+ * 
+ * Contact Information:
+ *
+ * Bjoern D. Rasmussen
+ * Kirketoften 5
+ * DK-8260 Viby J
+ * Denmark
+ * Email: contact@bearware.dk
+ * Phone: +45 20 20 54 59
+ * Web: http://www.bearware.dk
+ *
+ * This source code is part of the TeamTalk 5 SDK owned by
+ * BearWare.dk. All copyright statements may not be removed 
+ * or altered from any source distribution. If you use this
+ * software in a product, an acknowledgment in the product 
+ * documentation is required.
+ *
+ */
+
+package dk.bearware;
+
+public class AbusePrevention {
+    public int nCommandsLimit = 0;
+    public int nCommandsIntervalMSec = 0;
+    public AbusePrevention() {}
+}
