885f90fc057913fad9d4c1ee68a6c9bca717e5f4
Bjoern D. Rasmussen <contact@bearware.dk>
2017-05-25 05:20:43 +0200

    Version update

--- TeamTalk.NET/AssemblyInfo.cs
+++ TeamTalk.NET/AssemblyInfo.cs
@@ -35,5 +35,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("5.2.2.4890")]
+[assembly: AssemblyVersion("5.2.3.4891")]
 
--- TeamTalkClassic/AppInfo.h
+++ TeamTalkClassic/AppInfo.h
@@ -22,7 +22,7 @@
 #pragma once
 
 #define COMPANYNAME             _T("BearWare.dk")
-#define APPVERSION_SHORT        _T("5.2.2")
+#define APPVERSION_SHORT        _T("5.2.3")
 #define APPVERSION              _T( TEAMTALK_VERSION ) _T(" - Unreleased")
 
 #ifdef ENABLE_ENCRYPTION
