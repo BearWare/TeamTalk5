25f54dcba60d8281d9788e96a9f24fb2b585c291
Bjoern D. Rasmussen <contact@bearware.dk>
2017-07-27 08:55:06 +0200

    Tab-stop detection should get parent-parent, not main-parent

--- TeamTalkClassic/resource.h
+++ TeamTalkClassic/resource.h
@@ -773,9 +773,9 @@
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        292
+#define _APS_NEXT_RESOURCE_VALUE        301
 #define _APS_NEXT_COMMAND_VALUE         32950
 #define _APS_NEXT_CONTROL_VALUE         1551
-#define _APS_NEXT_SYMED_VALUE           292
+#define _APS_NEXT_SYMED_VALUE           301
 #endif
 #endif
