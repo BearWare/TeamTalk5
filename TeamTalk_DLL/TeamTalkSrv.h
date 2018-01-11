2aa97ec001c046e24e0d38c162ee77dbfd168edd
Bjoern D. Rasmussen <contact@bearware.dk>
2018-01-11 16:52:14 +0100

    Version 5.3.0.4909 beta

--- TeamTalk_DLL/TeamTalkSrv.h
+++ TeamTalk_DLL/TeamTalkSrv.h
@@ -18,7 +18,7 @@
  * client's version can be seen in the @a szVersion member of the
  * #User-struct. */
 
-#define TEAMTALK_SERVER_VERSION "5.3.0.4909"
+#define TEAMTALK_SERVER_VERSION "5.3.0.4910"
 
 #ifdef __cplusplus
 extern "C" {
