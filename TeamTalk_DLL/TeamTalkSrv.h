a9a96932e68d098f163e8df39af93836b2af84cc
Bjoern D. Rasmussen <contact@bearware.dk>
2018-02-18 11:49:54 +0100

    Fixed typo

--- TeamTalk_DLL/TeamTalkSrv.h
+++ TeamTalk_DLL/TeamTalkSrv.h
@@ -91,7 +91,7 @@ extern "C" {
                                           IN VOID* lpUserData,
                                           OUT ClientErrorMsg* lpClientErrorMsg,
                                           IN const User* lpUser, 
-                                          IN int nNewStatusMode,
+                                          IN INT32 nNewStatusMode,
                                           IN const TTCHAR* szNewStatusMsg);
     /**
      * @brief Callback when a user is requesting to create a new user
