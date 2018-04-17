32ee344940dfa82f5424a0cf9bd139c29c8ec7af
Bjørn Damstedt Rasmussen <contact@bearware.dk>
2018-04-17 22:32:40 +0200

    Version update

--- Library/TeamTalk_DLL/TeamTalkSrv.h
+++ Library/TeamTalk_DLL/TeamTalkSrv.h
@@ -18,7 +18,7 @@
  * client's version can be seen in the @a szVersion member of the
  * #User-struct. */
 
-#define TEAMTALK_SERVER_VERSION "5.3.2.4922"
+#define TEAMTALK_SERVER_VERSION "5.3.2.4923"
 
 #ifdef __cplusplus
 extern "C" {
