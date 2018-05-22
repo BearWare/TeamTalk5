cbcf938e353c59a407e0d6e601a37192dcddeaf6
Bjørn Damstedt Rasmussen <contact@bearware.dk>
2018-05-22 19:42:01 +0200

    Icon for TeamTalk Server

--- Library/TeamTalkLib/bin/ttsrv/resource.h
+++ Library/TeamTalkLib/bin/ttsrv/resource.h
@@ -1,35 +1,18 @@
-/*
- * Copyright (c) 2005-2018, BearWare.dk
- * 
- * Contact Information:
- *
- * Bjoern D. Rasmussen
- * Kirketoften 5
- * DK-8260 Viby J
- * Denmark
- * Email: contact@bearware.dk
- * Phone: +45 20 20 54 59
- * Web: http://www.bearware.dk
- *
- * This source code is part of the TeamTalk SDK owned by
- * BearWare.dk. Use of this file, or its compiled unit, requires a
- * TeamTalk SDK License Key issued by BearWare.dk.
- *
- * The TeamTalk SDK License Agreement along with its Terms and
- * Conditions are outlined in the file License.txt included with the
- * TeamTalk SDK distribution.
- *
- */
-
 //{{NO_DEPENDENCIES}}
 // Microsoft Visual C++ generated include file.
-// Used by teamtalk3d.rc
+// Used by ttsrv.rc
+//
+#define USER_TIMEOUT                    60
+#define IDR_MAINFRAME                   213
+#define MAX_STRING_LENGTH               511
+#define MAX_USERS                       1000
+#define TT_MAX_ID                       0xFFE
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        101
+#define _APS_NEXT_RESOURCE_VALUE        102
 #define _APS_NEXT_COMMAND_VALUE         40001
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
