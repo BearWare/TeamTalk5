94571f78032541c5c679b4379871309853222c8b
Bjørn Damstedt Rasmussen <contact@bearware.dk>
2019-01-04 13:59:29 +0100

    Forced load of HTTPs registration

--- Library/TeamTalkLib/test/UnitTestWin.cpp
+++ Library/TeamTalkLib/test/UnitTestWin.cpp
@@ -530,7 +530,7 @@ namespace UnitTest
             cv.wait(lk);
         }
 #endif
-        TEST_METHOD(HTTPS)
+        TEST_METHOD(TestHTTPS)
         {
             //ACE::HTTPS::Context::set_default_ssl_mode(ACE_SSL_Context::SSLv23);
             //ACE::HTTPS::Context::set_default_verify_mode(true);
@@ -539,9 +539,11 @@ namespace UnitTest
 
             std::string response1, response2, response3;
             Assert::AreEqual(1, HttpRequest("http://www.bearware.dk/teamtalk/weblogin.php?ping=1", response1));
+#if defined(ENABLE_ENCRYPTION)
             Assert::AreEqual(1, HttpRequest("https://www.bearware.dk/teamtalk/weblogin.php?ping=1", response2));
             Assert::AreEqual(response1, response2);
             Assert::AreEqual(1, HttpRequest("https://www.google.com", response3));
+#endif
         }
     };
 }
