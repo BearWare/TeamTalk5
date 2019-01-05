be3125f6a4bd71d5a0c1fa954e3e21aecc2329e3
Bjørn Damstedt Rasmussen <contact@bearware.dk>
2019-01-05 20:54:35 +0100

    Zero terminate string

--- Library/TeamTalkJNI/jni/ttconvert-jni.h
+++ Library/TeamTalkJNI/jni/ttconvert-jni.h
@@ -36,11 +36,11 @@
 
 #if defined(WIN32)
 #define NEW_JSTRING(env, str) (env->NewString(reinterpret_cast<const jchar*>(str), wcslen(str)))
-#define TT_STRCPY(dst, src) do { wcsncpy(dst, src, TT_STRLEN); dst[TT_STRLEN-1]; } while(0)
+#define TT_STRCPY(dst, src) do { wcsncpy(dst, src, TT_STRLEN); dst[TT_STRLEN-1] = 0; } while(0)
 const jint* TO_JINT_ARRAY(const INT32* ttints, jint* jints, INT32 N);
 #else
 #define NEW_JSTRING(env, str) (env->NewStringUTF(str))
-#define TT_STRCPY(dst, src) do { strncpy(dst, src, TT_STRLEN); dst[TT_STRLEN-1]; } while(0)
+#define TT_STRCPY(dst, src) do { strncpy(dst, src, TT_STRLEN); dst[TT_STRLEN-1] = 0; } while(0)
 #define TO_JINT_ARRAY(ttint32, jints, N) (ttint32)
 #endif
 
