b6e9da125602a113713bae206b63a4c65804a67c
Bjørn Damstedt Rasmussen <contact@bearware.dk>
2019-01-21 11:52:53 +0100

    Speex and SpeexDSP

--- Library/TeamTalkLib/build/speex.cmake
+++ Library/TeamTalkLib/build/speex.cmake
@@ -1,11 +1,20 @@
 
 if (MSVC)
-  set ( SPEEX_INCLUDE_DIR ${TTLIBS_ROOT}/speex/include )  
+  set (SPEEX_INCLUDE_DIR ${TTLIBS_ROOT}/speex/include)
 
-  set ( SPEEX_STATIC_LIB optimized ${TTLIBS_ROOT}/speex/lib/$(PlatformName)/libspeex_sse2.lib
+  set (SPEEX_LINK_FLAGS optimized ${TTLIBS_ROOT}/speex/lib/$(PlatformName)/libspeex_sse2.lib
     debug ${TTLIBS_ROOT}/speex/lib/$(PlatformName)/libspeexd.lib)
 
 else()
-  set ( SPEEX_INCLUDE_DIR ${TTLIBS_ROOT}/speex/include )
-  set ( SPEEX_STATIC_LIB ${TTLIBS_ROOT}/speex/lib/libspeex.a )
+
+  option (SPEEX_STATIC "Build using static Speex libraries" ON)
+
+  if (SPEEX_STATIC)
+    set ( SPEEX_INCLUDE_DIR ${TTLIBS_ROOT}/speex/include )
+    set ( SPEEX_STATIC_LIB ${TTLIBS_ROOT}/speex/lib/libspeex.a )
+  else()
+    # Ubuntu: libspeex-dev
+    find_library(SPEEX_LIBRARY speex)
+    set (SPEEX_LINK_FLAGS ${OPUS_LIBRARY})
+  endif()
 endif()
