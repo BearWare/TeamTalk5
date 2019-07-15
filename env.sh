b8c34645a886f9719e94d9d9bf581f7a6d579279
Bjørn Damstedt Rasmussen <contact@bearware.dk>
2019-07-15 22:18:50 +0200

    Release/debug targets for more platforms

--- env.sh
+++ env.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 
-export TEAMTALK_ROOT=$PWD
-
-if [ -e "$TEAMTALK_ROOT/Library/TeamTalkLib/toolchain/toolchain.sh" ]; then
-
-    cd $TEAMTALK_ROOT/Library/TeamTalkLib/toolchain
-    . toolchain.sh $@
-    cd -
-    
-fi
+TEAMTALK_ROOT=$(dirname ${BASH_SOURCE[0]})
+export TEAMTALK_ROOT
 
 if [ "Darwin" = `uname -s` ]; then
     export DYLD_LIBRARY_PATH=$TEAMTALK_ROOT/Library/TeamTalk_DLL:$TEAMTALK_ROOT/Library/TeamTalkJNI/libs
