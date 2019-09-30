21a03905423b2287a7dbabfd64503ed2505066e6
Bjørn Damstedt Rasmussen <contact@bearware.dk>
2019-09-30 19:29:04 +0200

    Version update

--- Client/TeamTalkClassic/AppInfo.h
+++ Client/TeamTalkClassic/AppInfo.h
@@ -24,7 +24,7 @@
 #pragma once
 
 #define COMPANYNAME             _T("BearWare.dk")
-#define APPVERSION_SHORT        _T("5.4.1")
+#define APPVERSION_SHORT        _T("5.5.0")
 #define APPVERSION              _T( TEAMTALK_VERSION ) _T(" - Unreleased")
 
 #ifdef ENABLE_TEAMTALKPRO
