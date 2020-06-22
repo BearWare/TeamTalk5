2418743c77f2c72e84abc687cdc38dc1a636251d
Oreonan <corentin@progaccess.net>
2020-06-22 21:30:50 +0200

    Remove tr function to settings.h

--- Client/qtTeamTalk/settings.h
+++ Client/qtTeamTalk/settings.h
@@ -31,7 +31,7 @@
 #define SETTINGS_GENERAL_VERSION_DEFAULT            "5.0"
 
 #define SETTINGS_GENERAL_NICKNAME                   "general_/nickname"
-#define SETTINGS_GENERAL_NICKNAME_DEFAULT           tr("NoName")
+#define SETTINGS_GENERAL_NICKNAME_DEFAULT           "NoName"
 #define SETTINGS_GENERAL_GENDER                     "general_/gender"
 #define SETTINGS_GENERAL_GENDER_DEFAULT             true
 #define SETTINGS_GENERAL_BEARWARE_USERNAME          "general_/bearwareid"
