c0e20ccbc2c7152a58a250b7cf3f7a72b6663ec1
Bjørn Damstedt Rasmussen <contact@bearware.dk>
2020-07-05 19:18:02 +0200

    Version update

--- Library/TeamTalk_DLL/TeamTalk.h
+++ Library/TeamTalk_DLL/TeamTalk.h
@@ -16,7 +16,7 @@
  * client's version can be seen in the @a szVersion member of the
  * #User-struct. */
 
-#define TEAMTALK_VERSION "5.6.0.5001"
+#define TEAMTALK_VERSION "5.6.0.5002"
 
 
 #if defined(WIN32)
