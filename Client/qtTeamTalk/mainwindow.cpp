0d9ce9472527ffb593beb67ab2e78cc05085a36e
Oreonan <corentin@progaccess.net>
2020-07-28 08:44:10 +0200

    Test with other function

--- Client/qtTeamTalk/mainwindow.cpp
+++ Client/qtTeamTalk/mainwindow.cpp
@@ -1028,7 +1028,7 @@ void MainWindow::processTTMessage(const TTMessage& msg)
         {
             updateChannelFiles(file.nChannelID);
             playSoundEvent(SOUNDEVENT_FILESUPD);
-            if(strlen(file.szUsername) > 0) {
+            if(!file.szUsername.empty()) {
                 TT_GetUserByUsername(ttInst, file.szUsername, &user);
                 addStatusMsg(tr("File %1 added by %2") .arg(file.szFileName).arg(getDisplayName(user)));
             }
