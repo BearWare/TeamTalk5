16cb1657c422e1158bfc06e2de857402f825f528
Oreonan <corentin@progaccess.net>
2020-08-01 21:00:35 +0200

    Add event for streaming media file start

--- Client/qtTeamTalk/mainwindow.cpp
+++ Client/qtTeamTalk/mainwindow.cpp
@@ -1131,6 +1131,13 @@ void MainWindow::processTTMessage(const TTMessage& msg)
                m_talking.size() == 0)
                 m_sysicon->setIcon(QIcon(APPTRAYICON_CON));
         }
+
+        if(user.uUserState & USERSTATE_MEDIAFILE_AUDIO && user.nUserID != TT_GetMyUserID(ttInst))
+        {
+            User nameuser;
+            TT_GetUser(ttInst, user.nUserID, &nameuser);
+            addStatusMsg(tr("%1 has started streaming mediafile to channel") .arg(getDisplayName(nameuser)));
+        }
         
         if(m_talking.empty())
             playSoundEvent(SOUNDEVENT_SILENCE);
