c5e23fe931b67236fd65313d13650169586b4544
Oreonan <corentin@progaccess.net>
2020-08-06 14:27:21 +0200

    Changes due to budget's reclamation

--- Client/qtTeamTalk/channelstree.cpp
+++ Client/qtTeamTalk/channelstree.cpp
@@ -969,11 +969,14 @@ void ChannelsTree::slotUpdateTreeWidgetItem(QTreeWidgetItem* item)
         if (user.nStatusMode & STATUSMODE_STREAM_MEDIAFILE)
             itemtext += tr(" (Streaming media file)");
 
-        if(TT_IsChannelOperator(ttInst, userid, ite->nChannelID))
+        if(TT_IsChannelOperator(ttInst, userid, ite->nChannelID) && !user.uUserType & USERTYPE_ADMIN)
             itemtext += tr(" [Channel operator]");
 
-        if(user.uUserType & USERTYPE_ADMIN)
-            itemtext += tr(" [Admin]");
+        if(user.uUserType & USERTYPE_ADMIN && !TT_IsChannelOperator(ttInst, userid, ite->nChannelID))
+            itemtext += tr(" [Administrator]");
+
+        if(user.uUserType & USERTYPE_ADMIN && TT_IsChannelOperator(ttInst, userid, ite->nChannelID))
+            itemtext += tr(" [Administrator, Channel operator]");
 
         if (itemtext.size() > m_strlen)
         {
