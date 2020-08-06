ced4f3fd22b80351b6b36ac78ae8a5cb21317023
Oreonan <corentin@progaccess.net>
2020-08-06 18:34:04 +0200

    Changes due to plooks reclamations

--- Client/qtTeamTalk/channelstree.cpp
+++ Client/qtTeamTalk/channelstree.cpp
@@ -969,11 +969,12 @@ void ChannelsTree::slotUpdateTreeWidgetItem(QTreeWidgetItem* item)
         if (user.nStatusMode & STATUSMODE_STREAM_MEDIAFILE)
             itemtext += tr(" (Streaming media file)");
 
-        if(TT_IsChannelOperator(ttInst, userid, ite->nChannelID))
-            itemtext += tr(" [Channel operator]");
-        
+      
         if(user.uUserType & USERTYPE_ADMIN)
             itemtext += tr(" [Administrator]");
+
+        if(TT_IsChannelOperator(ttInst, userid, ite->nChannelID))
+            itemtext += tr(" [Channel operator]");
 
         if (itemtext.size() > m_strlen)
         {
