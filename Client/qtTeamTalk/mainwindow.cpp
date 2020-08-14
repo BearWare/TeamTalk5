786000234992efecb372aa9c7067bac5d29a6f59
Oreonan <corentin@progaccess.net>
2020-08-14 20:49:43 +0200

    Second try

--- Client/qtTeamTalk/mainwindow.cpp
+++ Client/qtTeamTalk/mainwindow.cpp
@@ -3925,9 +3925,9 @@ void MainWindow::slotUsersAdvancedMediaFileAllowed(bool checked/*=false*/)
         QMap<int,StreamTypes> transmitUsers;
         ui.channelsWidget->getTransmitUsers(channelid, transmitUsers);
         if(checked)
-            transmitUsers[userid] |= STREAMTYPE_MEDIAFILE_AUDIO;
+            transmitUsers[userid] |= STREAMTYPE_MEDIAFILE_AUDIO | STREAMTYPE_MEDIAFILE_VIDEO;
         else
-            transmitUsers[userid] &= ~STREAMTYPE_MEDIAFILE_AUDIO;
+            transmitUsers[userid] &= ~STREAMTYPE_MEDIAFILE_AUDIO | ~STREAMTYPE_MEDIAFILE_VIDEO;
         slotTransmitUsersChanged(channelid, transmitUsers);
     }
 }
