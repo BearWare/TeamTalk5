bf2a55097951fe0ae3487c030be56f9986856b4b
Oreonan <corentin@progaccess.net>
2020-08-22 21:23:51 +0200

    Test

--- Client/qtTeamTalk/mainwindow.cpp
+++ Client/qtTeamTalk/mainwindow.cpp
@@ -3670,13 +3670,15 @@ void MainWindow::slotUsersMessages(bool /*checked =false */)
 void MainWindow::slotUsersMuteVoice(bool checked /*=false */)
 {
     foreach(int userid, ui.channelsWidget->selectedUsers()) {
-        User user;
+/*        User user;
         if( TT_GetUser(ttInst, userid, &user) )
         {
             TT_SetUserMute(ttInst, userid, STREAMTYPE_VOICE,
                            !(user.uUserState & USERSTATE_MUTE_VOICE));
         }
-        ui.actionMuteVoice->setChecked(checked);
+        ui.actionMuteVoice->setChecked(checked);*/
+        slotUsersMuteVoice(userid, checked);
+        slotUpdateUI()
     }
 }
 
