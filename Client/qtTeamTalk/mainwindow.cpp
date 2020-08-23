974936f21f2c1ab7ccca82fe05dcdebd5de4c3b8
Oreonan <corentin@progaccess.net>
2020-08-23 18:34:35 +0200

    Remove unexpected parenthesis

--- Client/qtTeamTalk/mainwindow.cpp
+++ Client/qtTeamTalk/mainwindow.cpp
@@ -1872,7 +1872,7 @@ void MainWindow::hotkeyToggle(HotKeyID id, bool active)
             } else {
                 playSoundEvent(SOUNDEVENT_VOICEACTOFF);
             }*/
-            slotMeEnableVoiceActivation());
+            slotMeEnableVoiceActivation();
         }
         break;
     case HOTKEY_INCVOLUME :
