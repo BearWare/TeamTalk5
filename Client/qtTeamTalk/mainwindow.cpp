dfeccde58bbfa397dca5a8496d4cdbf4ef4a2c61
Oreonan <corentin@progaccess.net>
2020-08-23 18:01:52 +0200

    Try to use another variable

--- Client/qtTeamTalk/mainwindow.cpp
+++ Client/qtTeamTalk/mainwindow.cpp
@@ -1867,7 +1867,8 @@ void MainWindow::hotkeyToggle(HotKeyID id, bool active)
         if(active) {
             TT_EnableVoiceActivation(ttInst, 
                      !(TT_GetFlags(ttInst) & CLIENT_SNDINPUT_VOICEACTIVATED));
-            if((TT_GetFlags(ttInst) & CLIENT_SNDINPUT_VOICEACTIVATED) == true) {
+//            if((TT_GetFlags(ttInst) & CLIENT_SNDINPUT_VOICEACTIVATED) == true) {
+            if(active == true) {
                 playSoundEvent(SOUNDEVENT_VOICEACTON);
             } else {
                 playSoundEvent(SOUNDEVENT_VOICEACTOFF);
