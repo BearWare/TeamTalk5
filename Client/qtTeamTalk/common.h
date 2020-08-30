128317a28da17f29440892cbfd30e1ecf8188588
Oreonan <corentin@progaccess.net>
2020-08-30 22:06:52 +0200

    Try to fix budget's problem

--- Client/qtTeamTalk/common.h
+++ Client/qtTeamTalk/common.h
@@ -453,4 +453,5 @@ void setMacResizeMargins(QDialog* dlg, QLayout* layout);
 
 void setCurrentItemData(QComboBox* cbox, const QVariant& itemdata);
 QVariant getCurrentItemData(QComboBox* cbox, const QVariant& not_found = QVariant());
+void slotUpdateUI();
 #endif
