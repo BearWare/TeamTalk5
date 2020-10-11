8a26d309c5f4f680cd17f1845e433127a6c8c4d2
Oreonan <corentin@progaccess.net>
2020-10-11 22:11:47 +0200

    Update h file

--- Client/qtTeamTalk/streammediafiledlg.h
+++ Client/qtTeamTalk/streammediafiledlg.h
@@ -43,6 +43,7 @@ public:
 private slots:
     void slotAccepted();
     void slotSelectFile();
+    void slotDeleteItem();
     void slotSelectionFile(const QString&);
     void showMediaFormatInfo();
     void updateControls();
