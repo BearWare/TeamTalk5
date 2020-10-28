d218e8cf5bcd00bc7456c33e746be7f69a809658
Bjørn Damstedt Rasmussen <contact@bearware.dk>
2020-10-28 20:52:33 +0100

    Build WebRTC for Linux. Seems ABSL macro is not needed.

--- Library/TeamTalkLib/build/webrtc.cmake
+++ Library/TeamTalkLib/build/webrtc.cmake
@@ -2,13 +2,6 @@ set (WEBRTC_INCLUDE_DIR ${TTLIBS_ROOT}/build/webrtc/src/modules/audio_processing
   ${TTLIBS_ROOT}/build/webrtc/src
   ${TTLIBS_ROOT}/build/webrtc/src/third_party/abseil-cpp)
 
-set (WEBRTC_COMPILE_FLAGS -DABSL_ALLOCATOR_NOTHROW=1)
-
-if (${CMAKE_SYSTEM_NAME} MATCHES "Darwin")
-  set (WEBRTC_LINK_FLAGS
+# $ find toolchain/webrtc/obj/ -name *.a -exec nm -A -C {} \; | grep webrtc::rnn_vad::RnnBasedVad::RnnBasedVad
+set (WEBRTC_LINK_FLAGS
     ${TTLIBS_ROOT}/webrtc/obj/modules/audio_processing/libteamtalk.a)
-else()
-  # Search for function definitions:
-  # $ find toolchain/webrtc/obj/ -name *.a -exec nm -A -C {} \; | grep webrtc::rnn_vad::RnnBasedVad::RnnBasedVad
-  set (WEBRTC_LINK_FLAGS ${TTLIBS_ROOT}/webrtc/obj/libwebrtc.a)
-endif()
