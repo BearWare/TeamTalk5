8aeb7eb8343843425252366ca6ffbc283173c113
Bjørn Damstedt Rasmussen <contact@bearware.dk>
2020-11-17 20:14:35 +0100

    WebRTC headers for Windows are now build/webrtc/include

--- Library/TeamTalkLib/build/webrtc.cmake
+++ Library/TeamTalkLib/build/webrtc.cmake
@@ -1,12 +1,12 @@
 if (MSVC)
   # Due to long file names webrtc must be installed in root folder.
   # This is caused by src/third_party/blink submodule
-  set (WEBRTC_DIR c:/webrtc/webrtc)
-  set (WEBRTC_INCLUDE_DIR ${WEBRTC_DIR}/src/modules/audio_processing/include
-    ${WEBRTC_DIR}/src ${WEBRTC_DIR}/src/third_party/abseil-cpp)
+  set (WEBRTC_INCLUDE_DIR ${TTLIBS_ROOT}/webrtc/include
+     ${TTLIBS_ROOT}/webrtc/include/modules/audio_processing/include
+     ${TTLIBS_ROOT}/webrtc/include/third_party/abseil-cpp)
   set (WEBRTC_COMPILE_FLAGS -DRTC_DISABLE_CHECK_MSG)
-  set (WEBRTC_LINK_FLAGS optimized ${TTLIBS_ROOT}/../webrtc/release/obj/modules/audio_processing/teamtalk.lib
-    debug ${TTLIBS_ROOT}/../webrtc/debug/obj/modules/audio_processing/teamtalk.lib)
+  set (WEBRTC_LINK_FLAGS optimized ${TTLIBS_ROOT}/webrtc/release/obj/modules/audio_processing/teamtalk.lib
+    debug ${TTLIBS_ROOT}/webrtc/debug/obj/modules/audio_processing/teamtalk.lib)
 else()
   if (${CMAKE_SYSTEM_NAME} MATCHES "Android")
     set (WEBRTC_INCLUDE_DIR ${TTLIBS_ROOT}/../build/webrtc/src/modules/audio_processing/include
