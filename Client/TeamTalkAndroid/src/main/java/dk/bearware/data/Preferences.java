1547c3e6a393a11a76225a62a691b8553bf7ba04
Bjørn Damstedt Rasmussen <contact@bearware.dk>
2021-01-24 21:26:43 +0100

    Gender support in Android client

--- Client/TeamTalkAndroid/src/main/java/dk/bearware/data/Preferences.java
+++ Client/TeamTalkAndroid/src/main/java/dk/bearware/data/Preferences.java
@@ -25,29 +25,30 @@ package dk.bearware.data;
 
 public class Preferences {
 
-	//duplicates of pref_connection.xml (isn't there an easier way to do this??)
-	public static final String 
-			PREF_GENERAL_NICKNAME = "nickname_text",
-			PREF_GENERAL_SHOWUSERNAMES = "showusernames_checkbox",
-			PREF_GENERAL_PUBLICSERVERS = "showpublicservers_checkbox",
-			PREF_GENERAL_BEARWARE_CHECKED = "bearwareid_checkbox",
-		    PREF_GENERAL_BEARWARE_USERNAME = "bearware_username",
-            PREF_GENERAL_BEARWARE_TOKEN = "bearware_token";
-	public static final String
-			PREF_SOUNDSYSTEM_MEDIAFILE_VOLUME = "mediafilevolume_seekbar",
-			PREF_SOUNDSYSTEM_VOICEACTIVATION_LEVEL = "voice_activation_level",
-			PREF_SOUNDSYSTEM_MASTERVOLUME = "mastervolume",
-			PREF_SOUNDSYSTEM_MICROPHONEGAIN = "microphonegain",
-			PREF_SOUNDSYSTEM_MUTE_ON_TRANSMISSION = "mute_speakers_on_tx_checkbox",
-			PREF_SOUNDSYSTEM_SPEAKERPHONE = "speakerphone_checkbox",
-			PREF_SOUNDSYSTEM_BLUETOOTH_HEADSET = "bluetooth_headset_checkbox",
-			PREF_SOUNDSYSTEM_VOICEPROCESSING = "voiceprocessing_checkbox";
-	public static final String
-			PREF_SUB_TEXTMESSAGE = "sub_txtmsg_checkbox",
-			PREF_SUB_CHANMESSAGE = "sub_chanmsg_checkbox",
-			PREF_SUB_BCAST_MESSAGES = "sub_bcastmsg_checkbox",
-			PREF_SUB_VOICE = "sub_voice_checkbox",
-			PREF_SUB_VIDCAP = "sub_video_checkbox",
-			PREF_SUB_DESKTOP = "sub_desktop_checkbox",
-			PREF_SUB_MEDIAFILE = "sub_mediafile_checkbox";
+    //duplicates of pref_connection.xml (isn't there an easier way to do this??)
+    public static final String 
+            PREF_GENERAL_NICKNAME = "nickname_text",
+            PREF_GENERAL_SHOWUSERNAMES = "showusernames_checkbox",
+            PREF_GENERAL_PUBLICSERVERS = "showpublicservers_checkbox",
+            PREF_GENERAL_BEARWARE_CHECKED = "bearwareid_checkbox",
+            PREF_GENERAL_BEARWARE_USERNAME = "bearware_username",
+            PREF_GENERAL_BEARWARE_TOKEN = "bearware_token",
+            PREF_GENERAL_GENDER = "gender_checkbox";
+    public static final String
+            PREF_SOUNDSYSTEM_MEDIAFILE_VOLUME = "mediafilevolume_seekbar",
+            PREF_SOUNDSYSTEM_VOICEACTIVATION_LEVEL = "voice_activation_level",
+            PREF_SOUNDSYSTEM_MASTERVOLUME = "mastervolume",
+            PREF_SOUNDSYSTEM_MICROPHONEGAIN = "microphonegain",
+            PREF_SOUNDSYSTEM_MUTE_ON_TRANSMISSION = "mute_speakers_on_tx_checkbox",
+            PREF_SOUNDSYSTEM_SPEAKERPHONE = "speakerphone_checkbox",
+            PREF_SOUNDSYSTEM_BLUETOOTH_HEADSET = "bluetooth_headset_checkbox",
+            PREF_SOUNDSYSTEM_VOICEPROCESSING = "voiceprocessing_checkbox";
+    public static final String
+            PREF_SUB_TEXTMESSAGE = "sub_txtmsg_checkbox",
+            PREF_SUB_CHANMESSAGE = "sub_chanmsg_checkbox",
+            PREF_SUB_BCAST_MESSAGES = "sub_bcastmsg_checkbox",
+            PREF_SUB_VOICE = "sub_voice_checkbox",
+            PREF_SUB_VIDCAP = "sub_video_checkbox",
+            PREF_SUB_DESKTOP = "sub_desktop_checkbox",
+            PREF_SUB_MEDIAFILE = "sub_mediafile_checkbox";
 }
