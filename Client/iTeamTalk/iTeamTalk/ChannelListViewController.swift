41cd723752a8680a258354c56b5138303cb52a48
Bjørn Damstedt Rasmussen <contact@bearware.dk>
2021-02-21 19:40:05 +0100

    Show kick dialogs in MainTabBarController

--- Client/iTeamTalk/iTeamTalk/ChannelListViewController.swift
+++ Client/iTeamTalk/iTeamTalk/ChannelListViewController.swift
@@ -883,30 +883,6 @@ class ChannelListViewController :
                 self.tableView.reloadData()
             }
             
-        case CLIENTEVENT_CMD_MYSELF_KICKED :
-            tableView.reloadData()
-            if (msg.nSource == 0)
-            {
-                playSound(.srv_LOST)
-                if #available(iOS 8.0, *) {
-                    let alert = UIAlertController(title: NSLocalizedString("Error", comment: "Dialog"), message: NSLocalizedString("You have been kicked from server", preferredStyle: UIAlertController.Style.alert)
-                    alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Dialog"), style: UIAlertAction.Style.default, handler: nil))
-                    self.present(alert, animated: true, completion: nil)
-                } else {
-                    // Fallback on earlier versions
-                }
-            }
-            else
-            {
-                if #available(iOS 8.0, *) {
-                    let alert = UIAlertController(title: NSLocalizedString("Error", comment: "Dialog"), message: NSLocalizedString("You have been kicked from channel", preferredStyle: UIAlertController.Style.alert)
-                    alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Dialog"), style: UIAlertAction.Style.default, handler: nil))
-                    self.present(alert, animated: true, completion: nil)
-                } else {
-                    // Fallback on earlier versions
-                }
-            }
-
         case CLIENTEVENT_CMD_USER_LOGGEDIN :
             let user = getUser(&m).pointee
             users[user.nUserID] = user
