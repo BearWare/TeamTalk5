BearWare/TeamTalk5

commit 2356b05364628d323f820b5bac61015b96854c25

QT - Select text of mediafileComboBox in streamMediaDlg at selection change

Client/qtTeamTalk/streammediafiledlg.cpp
@@ -145,6 +145,7 @@ void StreamMediaFileDlg::slotSelectFile()
 void StreamMediaFileDlg::slotSelectionFile(const QString&)
 {
     showMediaFormatInfo();
+    ui.mediafileComboBox->lineEdit()->setSelection(0, ui.mediafileComboBox->lineEdit()->text().size());
 }
 
 void StreamMediaFileDlg::showMediaFormatInfo()

commit 366c694bd539a0e6567bec689afb73d380347e7b

QT - Fixed issues caused by 2356b05 (#919)
* QT - Fixed issue caused by 2356b05

* Remove commented code used during tests

Co-authored-by: Bjørn Damstedt Rasmussen <bear101@users.noreply.github.com>

Client/qtTeamTalk/streammediafiledlg.cpp
@@ -30,6 +30,7 @@
 #include <QLineEdit>
 #include <QMessageBox>
 #include <QComboBox>
+#include <QKeyEvent>
 
 extern QSettings* ttSettings;
 extern TTInstance* ttInst;
@@ -94,6 +95,8 @@ StreamMediaFileDlg::StreamMediaFileDlg(QWidget* parent/* = 0*/)
     ui.mediafileComboBox->setCurrentIndex(0); // generates showMediaFormatInfo()
 
     updateControls();
+    ui.mediafileComboBox->installEventFilter(this);
+    ui.mediafileComboBox->lineEdit()->setCursorPosition(0);
 }
 
 StreamMediaFileDlg::~StreamMediaFileDlg()
@@ -145,7 +148,6 @@ void StreamMediaFileDlg::slotSelectFile()
 void StreamMediaFileDlg::slotSelectionFile(const QString&)
 {
     showMediaFormatInfo();
-    ui.mediafileComboBox->lineEdit()->setSelection(0, ui.mediafileComboBox->lineEdit()->text().size());
 }
 
 void StreamMediaFileDlg::showMediaFormatInfo()
@@ -421,4 +423,21 @@ void StreamMediaFileDlg::slotMediaPlaybackProgress(int sessionid, const MediaFil
     }
 
     m_progressupdate = false;
+}
+
+bool StreamMediaFileDlg::eventFilter(QObject *object, QEvent *event)
+{
+    if (object == ui.mediafileComboBox && event->type() == QEvent::KeyPress)
+    {
+        QKeyEvent *keyEvent = static_cast<QKeyEvent *>(event);
+        if (keyEvent->matches(QKeySequence::Paste) && ui.mediafileComboBox->currentIndex() == 0 && ui.mediafileComboBox->lineEdit()->cursorPosition() == 0)
+        {
+            ui.mediafileComboBox->lineEdit()->clear();
+            ui.mediafileComboBox->lineEdit()->paste();
+            return true;
+        }
+        else
+            return false;
+    }
+    return false;
 }

Client/qtTeamTalk/streammediafiledlg.h
@@ -54,6 +54,9 @@ private:
     void slotChangePreprocessor(int);
     void slotSetupPreprocessor(bool);
 
+protected:
+    bool eventFilter(QObject *object, QEvent *event);
+
 private:
     Ui::StreamMediaFileDlg ui;
     MediaFileInfo m_mediaFile = {};