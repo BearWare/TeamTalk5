baae94b6b7ac2dec4e926066fdf527e3b72d5ee1
Oreonan <corentin@progaccess.net>
2021-06-16 18:20:37 +0200

    Remove delete from history button

--- Client/qtTeamTalk/streammediafiledlg.h
+++ Client/qtTeamTalk/streammediafiledlg.h
@@ -43,7 +43,6 @@ public:
 private:
     void slotAccepted();
     void slotSelectFile();
-    void slotDeleteFromHistory();
     void slotClearHistory();
     void slotSelectionFile(const QString&);
     void showMediaFormatInfo();
