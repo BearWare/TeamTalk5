5b040788c01c719180b0682279086fda1d44f550
Bjørn Damstedt Rasmussen <contact@bearware.dk>
2021-08-11 21:04:19 +0200

    Build ACE, OpenSSL, TinyXML and ZLib as ExternalProject on macOS

--- Library/TeamTalkLib/build/openssl.cmake
+++ Library/TeamTalkLib/build/openssl.cmake
@@ -1,24 +1,19 @@
-# ACE is built using the static OpenSSL library from
-# ${TTLIBS_ROOT}/openssl on Windows, macOS, Android and iOS.
+# ACE is built using the static OpenSSL library from on Windows,
+# macOS, Android and iOS.
 #
-# On Linux distributions we build ACE from shared library by
+# On Linux distributions we build ACE using OpenSSL shared library by
 # linking libssl-dev package
 
-if (MSVC)
-  
-  set (OPENSSL_LINK_FLAGS openssl-ssl openssl-crypto)
-  
+if (${CMAKE_SYSTEM_NAME} MATCHES "Linux")
+
+  find_library(OPENSSL_SSL_LIBRARY ssl)
+  set (OPENSSL_LINK_FLAGS ${OPENSSL_SSL_LIBRARY})
+  find_library(OPENSSL_CRYPTO_LIBRARY crypto)
+  list (APPEND OPENSSL_LINK_FLAGS ${OPENSSL_CRYPTO_LIBRARY})
+  # Include path OPENSSL_INCLUDE_DIR is automatically set by CMake.
+
 else()
 
-  if (${CMAKE_SYSTEM_NAME} MATCHES "Linux")
-    find_library(OPENSSL_SSL_LIBRARY ssl)
-    set (OPENSSL_LINK_FLAGS ${OPENSSL_SSL_LIBRARY})
-    find_library(OPENSSL_CRYPTO_LIBRARY crypto)
-    list (APPEND OPENSSL_LINK_FLAGS ${OPENSSL_CRYPTO_LIBRARY})
-    # Include path OPENSSL_INCLUDE_DIR is automatically set by CMake.
-  else()
-    set (OPENSSL_INCLUDE_DIR ${TTLIBS_ROOT}/openssl/include)
-    set (OPENSSL_STATIC_LIB ${TTLIBS_ROOT}/openssl/lib/libssl.a ${TTLIBS_ROOT}/openssl/lib/libcrypto.a)
-    set (OPENSSL_LINK_FLAGS ${OPENSSL_STATIC_LIB})
-  endif()
+  set (OPENSSL_LINK_FLAGS openssl-ssl openssl-crypto)
+
 endif()
