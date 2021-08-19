a9b60c9ce726cee2f6312aefdaf209b924784617
Bjørn Damstedt Rasmussen <contact@bearware.dk>
2021-08-19 21:48:39 +0200

    Ubuntu 18 build using ExternalProject

--- Library/TeamTalkLib/build/openssl.cmake
+++ Library/TeamTalkLib/build/openssl.cmake
@@ -7,10 +7,16 @@
 if (${CMAKE_SYSTEM_NAME} MATCHES "Linux")
 
   find_library(OPENSSL_SSL_LIBRARY ssl)
+  if (NOT OPENSSL_SSL_LIBRARY)
+    message (FATAL_ERROR "OpenSSL SSL library not found")
+  endif()
   set (OPENSSL_LINK_FLAGS ${OPENSSL_SSL_LIBRARY})
+
   find_library(OPENSSL_CRYPTO_LIBRARY crypto)
+  if (NOT OPENSSL_CRYPTO_LIBRARY)
+    message (FATAL_ERROR "OpenSSL Crypto library not found")
+  endif()
   list (APPEND OPENSSL_LINK_FLAGS ${OPENSSL_CRYPTO_LIBRARY})
-  # Include path OPENSSL_INCLUDE_DIR is automatically set by CMake.
 
 else()
 
