ee57cf343acaacf01f124372b34c69dc024a6fbd
Bjørn Damstedt Rasmussen <contact@bearware.dk>
2021-08-30 20:17:56 +0200

    Configurable install location of WebRTC

--- Library/TeamTalkLib/build/webrtc.cmake
+++ Library/TeamTalkLib/build/webrtc.cmake
@@ -1,11 +1,4 @@
-if (MSVC)
-  set (WEBRTC_LINK_FLAGS webrtc)
-else()
-  set (WEBRTC_INCLUDE_DIR ${TTLIBS_ROOT}/webrtc/include
-    ${TTLIBS_ROOT}/webrtc/include/modules/audio_processing/include
-    ${TTLIBS_ROOT}/webrtc/include/third_party/abseil-cpp)
 
-  # $ find toolchain/webrtc/obj/ -name *.a -exec nm -A -C {} \; | grep webrtc::rnn_vad::RnnBasedVad::RnnBasedVad
-  set (WEBRTC_LINK_FLAGS
-    ${TTLIBS_ROOT}/webrtc/obj/modules/audio_processing/libteamtalk.a)
-endif()
+# WebRTC only supported through statically linked library
+set (WEBRTC_LINK_FLAGS webrtc)
+
