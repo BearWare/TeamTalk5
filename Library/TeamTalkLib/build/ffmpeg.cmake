bc9bf0c1a94d3736f1410fa7e3377d302ae72354
Bjørn Damstedt Rasmussen <contact@bearware.dk>
2021-09-04 08:20:03 +0200

    Move FFmpeg link options to FFmpeg CMakeLists.txt file

--- Library/TeamTalkLib/build/ffmpeg.cmake
+++ Library/TeamTalkLib/build/ffmpeg.cmake
@@ -1,13 +1,6 @@
 
 if (TOOLCHAIN_FFMPEG)
   set (FFMPEG_LINK_FLAGS ffmpeg-avdevice ffmpeg-avfilter ffmpeg-avformat ffmpeg-avcodec ffmpeg-swresample ffmpeg-swscale ffmpeg-avutil)
-
-  if ( ${CMAKE_SYSTEM_NAME} MATCHES "Linux" )
-    # Ubuntu: libasound2-dev
-    find_library(ASOUND_LIBRARY asound)
-    list (APPEND FFMPEG_LINK_FLAGS ${ASOUND_LIBRARY})
-  endif()
-  
 else()
   # Ubuntu: libavcodec-dev libavdevice-dev libavfilter-dev libavformat-dev libavresample-dev libavutil-dev libswresample-dev libswscale-dev
 
@@ -25,27 +18,4 @@ else()
   list (APPEND FFMPEG_LINK_FLAGS ${SWSCALE_LIBRARY})
   find_library(AVUTIL_LIBRARY avutil)
   list (APPEND FFMPEG_LINK_FLAGS ${AVUTIL_LIBRARY})
-endif()
-
-set (FFMPEG_COMPILE_FLAGS -D__STDC_CONSTANT_MACROS)
-
-if ( ${CMAKE_SYSTEM_NAME} MATCHES "Darwin" )
-  find_library(COCOA_LIBRARY Cocoa)
-  list (APPEND FFMPEG_LINK_FLAGS ${COCOA_LIBRARY})
-  find_library(AVFOUNDATION_LIBRARY AVFoundation)
-  list (APPEND FFMPEG_LINK_FLAGS ${AVFOUNDATION_LIBRARY})
-  find_library(COREMEDIA_LIBRARY CoreMedia)
-  list (APPEND FFMPEG_LINK_FLAGS ${COREMEDIA_LIBRARY})
-  find_library(OPENGL_LIBRARY OpenGL)
-  list (APPEND FFMPEG_LINK_FLAGS ${OPENGL_LIBRARY})
-  find_library(COREFOUNDATION_LIBRARY CoreFoundation)
-  list (APPEND FFMPEG_LINK_FLAGS ${COREFOUNDATION_LIBRARY})
-  find_library(VIDEODECODEACCELERATION_LIBRARY VideoDecodeAcceleration)
-  list (APPEND FFMPEG_LINK_FLAGS ${VIDEODECODEACCELERATION_LIBRARY})
-  find_library(QUARTZCORE_LIBRARY QuartzCore)
-  list (APPEND FFMPEG_LINK_FLAGS ${QUARTZCORE_LIBRARY})
-  find_library(BZ2_LIBRARY bz2)
-  list (APPEND FFMPEG_LINK_FLAGS ${BZ2_LIBRARY})
-  find_library (AUDIOTOOLBOX_LIBRARY AudioToolBox)
-  list (APPEND FFMPEG_LINK_FLAGS ${AUDIOTOOLBOX_LIBRARY})
 endif()
