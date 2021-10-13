98f98863ee119fdde4f2db818ec1642ec0cf1f41
Bjørn Damstedt Rasmussen <contact@bearware.dk>
2021-10-13 20:13:19 +0200

    Reenable OPUSFileSeek on Win32

--- Library/TeamTalkLib/test/CatchDefault.cpp
+++ Library/TeamTalkLib/test/CatchDefault.cpp
@@ -3722,14 +3722,6 @@ TEST_CASE("OPUSFileEncDec")
 
 TEST_CASE("OPUSFileSeek")
 {
-#if defined(WIN32)
-    if (GITHUBSKIP && sizeof(void*) == 4)
-    {
-        std::cout << "Skipping \"OPUSFileSeek\"... Does not work on 32-bit Windows" << std::endl;
-        return;
-    }
-#endif
-
     const auto SAMPLERATE = 12000;
     const auto FRAMESIZE_SEC = .04;
     const auto CHANNELS = 2;
