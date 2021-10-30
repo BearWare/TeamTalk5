7e617206075fbf1575be704b4bd1f9565ab9bb58
Bjørn Damstedt Rasmussen <contact@bearware.dk>
2021-10-30 12:31:35 +0200

    Version update

--- Library/TeamTalk.NET/AssemblyInfo.cs
+++ Library/TeamTalk.NET/AssemblyInfo.cs
@@ -58,5 +58,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("5.8.2.5055")]
+[assembly: AssemblyVersion("5.8.2.5056")]
 
--- Library/TeamTalk_DLL/TeamTalk.h
+++ Library/TeamTalk_DLL/TeamTalk.h
@@ -16,7 +16,7 @@
  * client's version can be seen in the @a szVersion member of the
  * #User-struct. */
 
-#define TEAMTALK_VERSION "5.8.2.5055"
+#define TEAMTALK_VERSION "5.8.2.5056"
 
 
 #if defined(WIN32)
