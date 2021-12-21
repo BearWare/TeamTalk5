eb2cc00867fcca6708d0ec7f6ef454d74af8f511
Patrick ZAJDA <patrick@zajda.fr>
2021-12-21 19:51:18 +0100

    iTeamtalk: set nickname to empty string if equal to nil

--- Client/iTeamTalk/iTeamTalk/PreferencesViewController.swift
+++ Client/iTeamTalk/iTeamTalk/PreferencesViewController.swift
@@ -123,9 +123,7 @@ class PreferencesViewController : UITableViewController, UITextFieldDelegate, Te
         
         var nickname = settings.string(forKey: PREF_GENERAL_NICKNAME)
         if nickname == nil {
-            nickname = DEFAULT_NICKNAME
-        if nickname == nil {
-            nickname = DEFAULT_NICKNAME
+            nickname = ""
         }
         
         // general items
