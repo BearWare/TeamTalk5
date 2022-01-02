2845df1342fb446525862d08ffe8e9a86a23e182
Bjørn Damstedt Rasmussen <contact@bearware.dk>
2022-01-02 20:34:26 +0100

    Version update

--- Library/TeamTalkLib/TeamTalkDefs.h
+++ Library/TeamTalkLib/TeamTalkDefs.h
@@ -24,9 +24,9 @@
 #if !defined(TEAMTALKDEFS_H)
 #define TEAMTALKDEFS_H
 
-#define TEAMTALK_VERSION            "5.8.3.5066"
-#define TEAMTALK_RC_VERSION         5,8,3,5066
-#define TEAMTALK_RC_FILEVERSION     "5, 8, 3, 5066"
+#define TEAMTALK_VERSION            "5.8.3.5067"
+#define TEAMTALK_RC_VERSION         5,8,3,5067
+#define TEAMTALK_RC_FILEVERSION     "5, 8, 3, 5067"
 #define TEAMTALK_VERSION_POSTFIX    " - Unreleased"
 
 #if defined(ENABLE_TEAMTALKPRO)
