78c24c160e87ab56cf118e2a336b1e535aeaa05f
Bjørn Damstedt Rasmussen <contact@bearware.dk>
2022-01-17 20:16:32 +0100

    Add extra nesting for CMAKE_SYSTEM_PROCESSOR

--- Library/TeamTalkLib/build/ace.cmake
+++ Library/TeamTalkLib/build/ace.cmake
@@ -37,6 +37,10 @@ if (${CMAKE_SYSTEM_NAME} MATCHES "Android")
 endif()
 
 # Special Raspberry Pi handling of 'off_t'
-if (${CMAKE_SYSTEM_NAME} MATCHES "Linux" AND ${CMAKE_SYSTEM_PROCESSOR} MATCHES "armv7l")
-  list (APPEND ACE_COMPILE_FLAGS -D_FILE_OFFSET_BITS=64)
+if (${CMAKE_SYSTEM_NAME} MATCHES "Linux")
+  # CMAKE_SYSTEM_PROCESSOR is unspecified on iOS so do extra nesting
+  # to avoid: "iOS" "MATCHES" "Linux" "AND" "MATCHES" "armv7l"
+  if (${CMAKE_SYSTEM_PROCESSOR} MATCHES "armv7l")
+    list (APPEND ACE_COMPILE_FLAGS -D_FILE_OFFSET_BITS=64)
+  endif()
 endif()
