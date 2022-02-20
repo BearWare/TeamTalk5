bcac23cd89adfb69a7df94d6e7524afb1fd3882f
Corentin Bacqué-Cazenave <corentin@progaccess.net>
2022-02-20 13:35:08 +0100

    Add accessible text

--- Client/qtTeamTalk/serverlistdlg.cpp
+++ Client/qtTeamTalk/serverlistdlg.cpp
@@ -105,6 +105,13 @@ QVariant ServerListModel::data(const QModelIndex & index, int role /*= Qt::Displ
         }
         break;
     case Qt::AccessibleTextRole :
+        switch (getServerType(getServers()[index.row()]))
+        {
+        case SERVERTYPE_LOCAL :
+            return QString(tr("Name: %1, Users: %2").arg(getServers()[index.row()].name).arg(getServers()[index.row()].usercount));
+        case SERVERTYPE_PUBLIC :
+            return QString(tr("Name: %1, Users: %2, Country: %3, MOTD: %4").arg(getServers()[index.row()].name).arg(getServers()[index.row()].usercount).arg(getServers()[index.row()].country).arg(getServers()[index.row()].motd));
+        }
         break;
     case Qt::ToolTipRole :
         return getServers()[index.row()].motd;
