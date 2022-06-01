da122b8414a4c75b93eeb7ab962bf32976d92f32
Corentin Bacqué-Cazenave <corentin@progaccess.net>
2022-06-01 16:04:47 +0200

    CheckState for TTSEventsModel

--- Client/qtTeamTalk/ttseventsmodel.cpp
+++ Client/qtTeamTalk/ttseventsmodel.cpp
@@ -195,10 +195,16 @@ QVariant TTSEventsModel::data ( const QModelIndex & index, int role /*= Qt::Disp
         case TTS_NONE :
             break;
         }
-    case Qt::AccessibleTextRole :
-        return QString("%1: %2").arg(data(index, Qt::DisplayRole).toString()).arg((m_ttsselected & m_ttsevents[index.row()])? tr("Enabled") : tr("Disabled"));
+    case Qt::CheckStateRole :
+        return (m_ttsselected & m_ttsevents[index.row()])? Qt::Checked : Qt::Unchecked;
     }
     return QVariant();
+}
+
+Qt::ItemFlags TTSEventsModel::flags(const QModelIndex &index) const
+{
+    if (index.isValid())
+        return Qt::ItemIsEnabled | Qt::ItemIsSelectable | Qt::ItemIsUserCheckable |  Qt::ItemIsEditable;
 }
 
 QModelIndex TTSEventsModel::index ( int row, int column, const QModelIndex & /*parent*/ /*= QModelIndex()*/ ) const
