2ce21938e472aaeb4b3de6131638f749de28a75d
Bjørn Damstedt Rasmussen <contact@bearware.dk>
2022-06-15 08:15:28 +0200

    Replace $(PlatformName) with ${CMAKE_VS_PLATFORM_NAME}
    This is in order to support ninja builds on Windows using Visual
    Studio CMake generator.

--- Library/TeamTalkLib/build/dshow.cmake
+++ Library/TeamTalkLib/build/dshow.cmake
@@ -3,6 +3,6 @@
 if (MSVC)
   set ( DSHOW_INCLUDE_DIR ${TTLIBS_ROOT}/DirectShow/BaseClasses )
 
-  set ( DSHOW_STATIC_LIB optimized ${TTLIBS_ROOT}/DirectShow/BaseClasses/lib/$(PlatformName)/strmbase.lib
-    debug ${TTLIBS_ROOT}/DirectShow/BaseClasses/lib/$(PlatformName)/strmbased.lib)
+  set ( DSHOW_STATIC_LIB optimized ${TTLIBS_ROOT}/DirectShow/BaseClasses/lib/${CMAKE_VS_PLATFORM_NAME}/strmbase.lib
+    debug ${TTLIBS_ROOT}/DirectShow/BaseClasses/lib/${CMAKE_VS_PLATFORM_NAME}/strmbased.lib)
 endif()
--- Library/TeamTalkLib/build/vidcap.cmake
+++ Library/TeamTalkLib/build/vidcap.cmake
@@ -1,7 +1,7 @@
 if (MSVC)
   set ( VIDCAP_INCLUDE_DIR ${TTLIBS_ROOT}/libvidcap-dsbaseclasses/include )
 
-  set ( VIDCAP_STATIC_LIB optimized ${TTLIBS_ROOT}/libvidcap-dsbaseclasses/lib/$(PlatformName)/libvidcap.lib
-    debug ${TTLIBS_ROOT}/libvidcap-dsbaseclasses/lib/$(PlatformName)/libvidcapd.lib)
+  set ( VIDCAP_STATIC_LIB optimized ${TTLIBS_ROOT}/libvidcap-dsbaseclasses/lib/${CMAKE_VS_PLATFORM_NAME}/libvidcap.lib
+    debug ${TTLIBS_ROOT}/libvidcap-dsbaseclasses/lib/${CMAKE_VS_PLATFORM_NAME}/libvidcapd.lib)
 
 endif()
