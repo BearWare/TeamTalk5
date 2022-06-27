dc077accc4a8b36cc891bff50661bfb2771a5d16
Corentin Bacqué-Cazenave <corentin@progaccess.net>
2022-06-27 20:31:24 +0200

    Option to disable voice activation when inactive (#1362)
    * Option to disable voice activation when inactive
    
    * Rename checkbox
    
    * Don't check isMyselfTalking
    
    * Don't check isMyselfTalking() when switching to online mode
    
    Co-authored-by: Bjørn Damstedt Rasmussen <bear101@users.noreply.github.com>

--- Client/qtTeamTalk/preferencesdlg.cpp
+++ Client/qtTeamTalk/preferencesdlg.cpp
@@ -499,6 +499,7 @@ void PreferencesDlg::slotTabChange(int index)
 
         ui.awaySpinBox->setValue(ttSettings->value(SETTINGS_GENERAL_AUTOAWAY, SETTINGS_GENERAL_AUTOAWAY_DEFAULT).toInt());
         slotUpdateASBAccessibleName();
+        ui.disableVoiceActCheckBox->setChecked(ttSettings->value(SETTINGS_GENERAL_INACTIVITY_DISABLE_VOICEACT, SETTINGS_GENERAL_INACTIVITY_DISABLE_VOICEACT_DEFAULT).toBool());
         ui.pttChkBox->setChecked(ttSettings->value(SETTINGS_GENERAL_PUSHTOTALK).toBool());
         ui.pttlockChkBox->setChecked(ttSettings->value(SETTINGS_GENERAL_PUSHTOTALKLOCK,
                                                        SETTINGS_GENERAL_PUSHTOTALKLOCK_DEFAULT).toBool());
@@ -808,6 +809,7 @@ void PreferencesDlg::slotSaveChanges()
         else
             ttSettings->setValue(SETTINGS_GENERAL_GENDER, GENDER_NEUTRAL);
         ttSettings->setValue(SETTINGS_GENERAL_AUTOAWAY, ui.awaySpinBox->value());
+        ttSettings->setValue(SETTINGS_GENERAL_INACTIVITY_DISABLE_VOICEACT, ui.disableVoiceActCheckBox->isChecked());
         saveHotKeySettings(HOTKEY_PUSHTOTALK, m_hotkey);
         ttSettings->setValue(SETTINGS_GENERAL_PUSHTOTALK, ui.pttChkBox->isChecked());
         ttSettings->setValue(SETTINGS_GENERAL_PUSHTOTALKLOCK, ui.pttlockChkBox->isChecked());
