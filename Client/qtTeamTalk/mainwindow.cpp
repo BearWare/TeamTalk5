dd71e63f9862b511bd7340fd42c7e5cf0e88bdba
Corentin Bacqué-Cazenave <corentin@progaccess.net>
2022-11-03 13:37:58 +0100

    Only play "Channel silent" sound event in channels

--- Client/qtTeamTalk/mainwindow.cpp
+++ Client/qtTeamTalk/mainwindow.cpp
@@ -1357,7 +1357,7 @@ void MainWindow::clienteventUserStateChange(const User& user)
         addStatusMsg(STATUSBAR_BYPASS, tr("Streaming from %1 started") .arg(getDisplayName(nameuser)));
     }
 
-    if(m_talking.empty())
+    if(m_talking.empty() && TT_GetMyChannelID(ttInst) > 0)
         playSoundEvent(SOUNDEVENT_SILENCE);
 }
 
