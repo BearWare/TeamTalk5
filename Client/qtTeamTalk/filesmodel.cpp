60661f90c64dd9571c276241405c4fb3671b624f
jeremyp3 <jeremyp3@github.com>
2023-01-18 21:46:07 +0100

    Add G size hint to file list (#1730)

--- Client/qtTeamTalk/filesmodel.cpp
+++ Client/qtTeamTalk/filesmodel.cpp
@@ -82,7 +82,9 @@ QVariant FilesModel::data ( const QModelIndex & index, int role /*= Qt::DisplayR
         case COLUMN_INDEX_SIZE :
             {
                 QString result;
-                if(m_files[index.row()].nFileSize>=1024*1024)
+                if(m_files[index.row()].nFileSize>=1024*1024*1024)
+                    result = QString("%1 G").arg(m_files[index.row()].nFileSize/(1024*1024*1024));
+                else if(m_files[index.row()].nFileSize>=1024*1024)
                     result = QString("%1 M").arg(m_files[index.row()].nFileSize/(1024*1024));
                 else if(m_files[index.row()].nFileSize>=1024)
                     result = QString("%1 K").arg(m_files[index.row()].nFileSize/1024);
@@ -99,7 +101,9 @@ QVariant FilesModel::data ( const QModelIndex & index, int role /*= Qt::DisplayR
     case Qt::AccessibleTextRole :
     {
         QString result;
-        if(m_files[index.row()].nFileSize>=1024*1024)
+        if(m_files[index.row()].nFileSize>=1024*1024*1024)
+            result = QString("%1 G").arg(m_files[index.row()].nFileSize/(1024*1024*1024));
+         else if(m_files[index.row()].nFileSize>=1024*1024)
             result = QString("%1 M").arg(m_files[index.row()].nFileSize/(1024*1024));
         else if(m_files[index.row()].nFileSize>=1024)
             result = QString("%1 K").arg(m_files[index.row()].nFileSize/1024);
