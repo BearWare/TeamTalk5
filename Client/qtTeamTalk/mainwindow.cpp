008b9bafcf1f40805e366b41de4572cf68d52368
Corentin Bacqué-Cazenave <corentin@progaccess.net>
2023-03-26 23:31:12 +0200

    Update UI when enabling accessibility options at first startup

--- Client/qtTeamTalk/mainwindow.cpp
+++ Client/qtTeamTalk/mainwindow.cpp
@@ -820,6 +820,7 @@ void MainWindow::loadSettings()
     }
 
     slotUpdateUI();
+
     startTTS();
 
     if ((ttSettings->value(SETTINGS_DISPLAY_START_SERVERLIST, SETTINGS_DISPLAY_START_SERVERLIST_DEFAULT).toBool() == true && ttSettings->value(SETTINGS_CONNECTION_AUTOCONNECT, SETTINGS_CONNECTION_AUTOCONNECT_DEFAULT).toBool() == false) && ((TT_GetFlags(ttInst) & CLIENT_CONNECTION) == CLIENT_CLOSED))
