199771cbf2c6db5b331a92a68d78b39bc760b00b
Patrick ZAJDA <patrick@zajda.fr>
2023-05-10 18:38:13 +0200

    Detect screen reader on Linux and ask user to choose more accessible settings
    Signed-off-by: Patrick ZAJDA <patrick@zajda.fr>

--- Client/qtTeamTalk/mainwindow.cpp
+++ Client/qtTeamTalk/mainwindow.cpp
@@ -78,7 +78,8 @@
 #include <QTextToSpeech>
 #endif
 
-#ifdef Q_OS_LINUX //For hotkeys on X11
+#ifdef Q_OS_LINUX //For DBus and hotkeys on X11
+#include <QtDBus/QtDBus>
 #include <X11/Xlib.h>
 #include <X11/Xutil.h>
 #endif
@@ -753,15 +754,23 @@ void MainWindow::loadSettings()
 
     if (ttSettings->value(SETTINGS_GENERAL_FIRSTSTART, SETTINGS_GENERAL_FIRSTSTART_DEFAULT).toBool())
     {
+#if (defined(Q_OS_WINDOWS) && defined(ENABLE_TOLK)) || defined(Q_OS_LINUX)
+    bool SRActive = false;
 #if defined(Q_OS_WINDOWS) && defined(ENABLE_TOLK)
     bool tolkLoaded = Tolk_IsLoaded();
     if (!tolkLoaded)
         Tolk_Load();
 
-    bool SRActive = Tolk_DetectScreenReader() != nullptr;
+    SRActive = Tolk_DetectScreenReader() != nullptr;
 
     if (!tolkLoaded)
         Tolk_Unload();
+#elif defined(Q_OS_LINUX)
+    QDBusInterface interface("org.a11y.Bus", "/org/a11y/bus", "org.a11y.Status", QDBusConnection::sessionBus());
+    if (interface.isValid()) {
+        SRActive = interface.property("ScreenReaderEnabled").toBool();
+    }
+#endif
 
     if (SRActive)
     {
@@ -776,7 +785,11 @@ void MainWindow::loadSettings()
 
         if(answer.clickedButton() == YesButton)
         {
+#if defined(Q_OS_WINDOWS)
             ttSettings->setValue(SETTINGS_TTS_ENGINE, TTSENGINE_TOLK);
+#elif defined(Q_OS_LINUX)
+            ttSettings->setValue(SETTINGS_TTS_ENGINE, QFile::exists(TTSENGINE_NOTIFY_PATH)?TTSENGINE_NOTIFY:TTSENGINE_QT);
+#endif
             ttSettings->setValue(SETTINGS_DISPLAY_VU_METER_UPDATES, false);
         }
     }
