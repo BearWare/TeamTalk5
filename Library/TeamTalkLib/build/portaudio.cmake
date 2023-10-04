4ba4a77ac54e99f0cf10f05d33148e5b472e98ac
Bjørn Damstedt Rasmussen <contact@bearware.dk>
2023-10-04 14:14:36 +0200

    Set PortAudio dependencies on toolchain's STATIC IMPORTED library
    find_library is now performed in build/portaudio/CMakeLists.txt
    instead of build/portaudio.cmake.

--- Library/TeamTalkLib/build/portaudio.cmake
+++ Library/TeamTalkLib/build/portaudio.cmake
@@ -9,23 +9,3 @@ else()
   set (PORTAUDIO_LINK_FLAGS ${PORTAUDIO_LIBRARY})
 
 endif()
-
-if ( ${CMAKE_SYSTEM_NAME} MATCHES "Darwin" )
-  find_library (CARBON_LIBRARY Carbon)
-  list (APPEND PORTAUDIO_LINK_FLAGS ${CARBON_LIBRARY})
-  find_library (AUDIOUNIT_LIBRARY AudioUnit)
-  list (APPEND PORTAUDIO_LINK_FLAGS ${AUDIOUNIT_LIBRARY} )
-  find_library (COREAUDIO_LIBRARY CoreAudio)
-  list (APPEND PORTAUDIO_LINK_FLAGS ${COREAUDIO_LIBRARY} )
-  find_library (AUDIOTOOLBOX_LIBRARY AudioToolBox )
-  list (APPEND PORTAUDIO_LINK_FLAGS ${AUDIOTOOLBOX_LIBRARY} )
-endif()
-
-if ( ${CMAKE_SYSTEM_NAME} MATCHES "Linux" )
-  # Ubuntu: libasound2-dev
-  find_library(ASOUND_LIBRARY asound)
-  list (APPEND PORTAUDIO_LINK_FLAGS ${ASOUND_LIBRARY})
-  # Ubuntu: libpulse-dev
-  find_library(PULSE_LIBRARY pulse)
-  list (APPEND PORTAUDIO_LINK_FLAGS ${PULSE_LIBRARY})
-endif()
