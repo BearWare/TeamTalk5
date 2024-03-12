507933b9fcc9ab4c29747afea5b5b9f798ef1522
Corentin BACQUÉ-CAZENAVE <corentin@progaccess.net>
2024-03-12 20:11:10 +0100

    Apply rquested changes

--- Client/qtTeamTalk/mainwindow.cpp
+++ Client/qtTeamTalk/mainwindow.cpp
@@ -747,48 +747,48 @@ void MainWindow::loadSettings()
     if(connect_ok)
         QTimer::singleShot(0, this, &MainWindow::slotConnectToLatest);
 
-#if (defined(Q_OS_WINDOWS) && defined(ENABLE_TOLK)) || defined(Q_OS_LINUX)
+#if defined(ENABLE_TOLK) || defined(Q_OS_LINUX)
     if (ttSettings->value(SETTINGS_GENERAL_FIRSTSTART, SETTINGS_GENERAL_FIRSTSTART_DEFAULT).toBool())
     {
-    bool SRActive = false;
-#if defined(Q_OS_WINDOWS) && defined(ENABLE_TOLK)
-    bool tolkLoaded = Tolk_IsLoaded();
-    if (!tolkLoaded)
-        Tolk_Load();
+        bool SRActive = false;
+#if defined(ENABLE_TOLK)
+        bool tolkLoaded = Tolk_IsLoaded();
+        if (!tolkLoaded)
+            Tolk_Load();
 
-    SRActive = Tolk_DetectScreenReader() != nullptr;
+        SRActive = Tolk_DetectScreenReader() != nullptr;
 
-    if (!tolkLoaded)
-        Tolk_Unload();
+        if (!tolkLoaded)
+            Tolk_Unload();
 #elif defined(Q_OS_LINUX)
-    QDBusInterface interface("org.a11y.Bus", "/org/a11y/bus", "org.a11y.Status", QDBusConnection::sessionBus());
-    if (interface.isValid())
-    {
-        SRActive = interface.property("ScreenReaderEnabled").toBool();
-    }
-#endif
-    if (SRActive)
-    {
-        QMessageBox answer;
-        answer.setText(tr("%1 has detected usage of a screenreader on your computer. Do you wish to enable accessibility options offered by %1 with recommended settings?").arg(APPNAME_SHORT));
-        QAbstractButton *YesButton = answer.addButton(tr("&Yes"), QMessageBox::YesRole);
-        QAbstractButton *NoButton = answer.addButton(tr("&No"), QMessageBox::NoRole);
-        Q_UNUSED(NoButton);
-        answer.setIcon(QMessageBox::Question);
-        answer.setWindowTitle(APPNAME_SHORT);
-        answer.exec();
-
-        if(answer.clickedButton() == YesButton)
+        QDBusInterface interface("org.a11y.Bus", "/org/a11y/bus", "org.a11y.Status", QDBusConnection::sessionBus());
+        if (interface.isValid())
         {
-#if defined(Q_OS_WINDOWS)
-            ttSettings->setValue(SETTINGS_TTS_ENGINE, TTSENGINE_TOLK);
-#elif defined(Q_OS_LINUX)
-            ttSettings->setValue(SETTINGS_TTS_ENGINE, QFile::exists(TTSENGINE_NOTIFY_PATH)?TTSENGINE_NOTIFY:TTSENGINE_QT);
-#endif
-            ttSettings->setValue(SETTINGS_DISPLAY_VU_METER_UPDATES, false);
+            SRActive = interface.property("ScreenReaderEnabled").toBool();
         }
-    }
-        ttSettings->setValue(SETTINGS_GENERAL_FIRSTSTART, false);
+#endif
+        if (SRActive)
+        {
+            QMessageBox answer;
+            answer.setText(tr("%1 has detected usage of a screenreader on your computer. Do you wish to enable accessibility options offered by %1 with recommended settings?").arg(APPNAME_SHORT));
+            QAbstractButton *YesButton = answer.addButton(tr("&Yes"), QMessageBox::YesRole);
+            QAbstractButton *NoButton = answer.addButton(tr("&No"), QMessageBox::NoRole);
+            Q_UNUSED(NoButton);
+            answer.setIcon(QMessageBox::Question);
+            answer.setWindowTitle(APPNAME_SHORT);
+            answer.exec();
+
+            if(answer.clickedButton() == YesButton)
+            {
+#if defined(ENABLE_TOLK)
+                ttSettings->setValue(SETTINGS_TTS_ENGINE, TTSENGINE_TOLK);
+#elif defined(Q_OS_LINUX)
+                ttSettings->setValue(SETTINGS_TTS_ENGINE, QFile::exists(TTSENGINE_NOTIFY_PATH)?TTSENGINE_NOTIFY:TTSENGINE_QT);
+#endif
+                ttSettings->setValue(SETTINGS_DISPLAY_VU_METER_UPDATES, false);
+            }
+        }
+            ttSettings->setValue(SETTINGS_GENERAL_FIRSTSTART, false);
     }
 #endif
 
