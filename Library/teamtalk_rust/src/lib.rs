cdf563abc495d91639cc5fad53bbb0b64052854e
Bjørn Damstedt Rasmussen <contact@bearware.dk>
2024-03-24 21:48:31 +0100

    WIP: wait_for_event

--- Library/teamtalk_rust/src/lib.rs
+++ Library/teamtalk_rust/src/lib.rs
@@ -5,7 +5,9 @@
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
-use std::ffi::CString;
+use std::time::{Duration, Instant};
+use std::{ffi::CString, ptr};
+use std::result::Result;
 
 pub fn add(left: usize, right: usize) -> usize {
     left + right
@@ -13,6 +15,32 @@ pub fn add(left: usize, right: usize) -> usize {
 
 fn ttstr(str: &str) -> CString {
     CString::new(str).unwrap_or(CString::new("").unwrap())
+}
+
+fn wait_for_event(ttinst: *mut TTInstance, event: ClientEvent, def_wait: Duration) -> Result<TTMessage, ClientEvent> {
+    let mut m = TTMessage::default();
+    let start = Instant::now();
+    unsafe {
+        let mut remain_msec: INT32  = def_wait.as_millis().try_into().expect("Duration is too high");
+        while TT_GetMessage(ttinst, &mut m, &remain_msec) == TRUE || def_wait <= Instant::now() - start {
+            if m.nClientEvent == event {
+                return Ok(m)
+            }
+            remain_msec = (def_wait - (Instant::now() - start)).as_millis().try_into().expect("Duration is too high");
+        }
+    }
+    Err(event)
+}
+
+fn wait_for_cmdsuccess(ttinst: *mut TTInstance, cmdid: INT32, def_wait: Duration) -> Result<TTMessage, ClientEvent> {
+    loop {
+        match wait_for_event(ttinst, ClientEvent::CLIENTEVENT_CMD_SUCCESS, def_wait) {
+            Ok(m) => if m.nSource == cmdid {
+                return Ok(m)
+            }
+            Err(e) => return Err(e)
+        }
+    }
 }
 
 #[cfg(test)]
@@ -44,16 +72,15 @@ mod tests {
     #[test]
     fn login() {
         unsafe {
+            let DEF_WAIT = Duration::new(5, 0);
             let a = TT_InitTeamTalkPoll();
             assert!(TT_Connect(a, ttstr("192.168.0.110").as_ptr(), 10333, 10333, 0, 0, FALSE) == TRUE);
             let c = Channel::default();
             let mut m = TTMessage::default();
-            assert!(TT_GetMessage(a, &mut m, ptr::null()) == TRUE);
-            assert_eq!(m.nClientEvent, ClientEvent::CLIENTEVENT_CON_SUCCESS);
+            assert!(wait_for_event(a, ClientEvent::CLIENTEVENT_CON_SUCCESS, DEF_WAIT).is_ok());
             let cmdid = TT_DoLogin(a, ttstr("foobar").as_ptr(), ttstr("guest").as_ptr(), ttstr("guest").as_ptr());
             assert!(cmdid > 0);
-            assert!(TT_GetMessage(a, &mut m, ptr::null()) == TRUE);
-            assert_eq!(m.nClientEvent, ClientEvent::CLIENTEVENT_CMD_MYSELF_LOGGEDIN);
+            assert!(wait_for_cmdsuccess(a, cmdid, DEF_WAIT).is_ok());
             TT_CloseTeamTalk(a);
         }
     }
