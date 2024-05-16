002de01899a6eb7c67eadf881f2b537df551b040
Corentin BACQUÉ-CAZENAVE <corentin@progaccess.net>
2024-05-16 18:51:11 +0200

    Fix build on Qt5

--- Client/qtTeamTalk/utiltts.h
+++ Client/qtTeamTalk/utiltts.h
@@ -88,7 +88,7 @@ typedef qulonglong TTSEvents;
 struct TTSEventInfo {
     QString settingKey;
     QHash<QString, QString> variables;
-    QString eventName = QString("");
+    QString eventName;
 };
 
 enum TextToSpeechEngine
