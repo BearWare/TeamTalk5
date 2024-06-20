be62241da1a8866f508529dcbe575931c851816d
Corentin BACQUÉ-CAZENAVE <corentin@progaccess.net>
2024-06-20 11:14:36 +0200

    Fix AnnouncementObject definition

--- Client/qtTeamTalk/utiltts.cpp
+++ Client/qtTeamTalk/utiltts.cpp
@@ -127,6 +127,7 @@ void addTextToSpeechMessage(const QString& msg)
         break;
     }
     case TTSENGINE_QTANNOUNCEMENT:
+    {
 #if QT_VERSION >= QT_VERSION_CHECK(6,8,0)
         QAccessibleAnnouncementEvent announcementEvent(announcerObject, msg);
         if (ttSettings->value(SETTINGS_TTS_ASSERTIVE, SETTINGS_TTS_ASSERTIVE_DEFAULT).toBool() == true)
@@ -134,6 +135,7 @@ void addTextToSpeechMessage(const QString& msg)
         QAccessible::updateAccessibility(&announcementEvent);
 #endif
         break;
+    }
     case TTSENGINE_APPLESCRIPT:
 #if defined(Q_OS_MAC)
         QString appleScript = QString(R"(
