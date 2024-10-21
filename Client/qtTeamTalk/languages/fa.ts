298ce0c48fccc06bb05ddf355f93fa0c0a1db5c5
Corentin BACQUÉ-CAZENAVE <corentin@progaccess.net>
2024-10-21 08:13:36 +0200

    Update Persian Qt translation

--- Client/qtTeamTalk/languages/fa.ts
+++ Client/qtTeamTalk/languages/fa.ts
@@ -8581,7 +8581,7 @@ Delete the published user account to unregister your server.</source>
     <message>
         <location filename="../useraccountdlg.cpp" line="373"/>
         <source>Last login: %1</source>
-        <translation type="unfinished"></translation>
+        <translation>آخرین ورود: %1</translation>
     </message>
     <message>
         <location filename="../useraccountdlg.cpp" line="438"/>
@@ -8659,7 +8659,7 @@ Delete the published user account to unregister your server.</source>
     <message>
         <location filename="../useraccountsdlg.cpp" line="198"/>
         <source>&amp;Last Login Time (%1)</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;آخرین زمان ورود (%1)</translation>
     </message>
     <message>
         <location filename="../useraccountsdlg.cpp" line="203"/>
@@ -8722,7 +8722,7 @@ Delete the published user account to unregister your server.</source>
     <message>
         <location filename="../useraccountsmodel.cpp" line="41"/>
         <source>Last Login</source>
-        <translation type="unfinished"></translation>
+        <translation>آخرین ورود</translation>
     </message>
     <message>
         <location filename="../useraccountsmodel.cpp" line="67"/>
