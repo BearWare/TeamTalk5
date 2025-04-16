6c44c061bb89a262d7c38ab9054c6861582fe80d
Bjørn Damstedt Rasmussen <contact@bearware.dk>
2025-04-16 20:56:03 +0200

    Fix tab order so send button is not first

--- Client/qtTeamTalk/mainwindow.cpp
+++ Client/qtTeamTalk/mainwindow.cpp
@@ -2776,7 +2776,11 @@ void MainWindow::setupChatHistory()
         auto index = layout->indexOf(ui.chatEdit);
         layout->removeWidget(ui.chatEdit);
         layout->insertWidget(index, chat);
+// hm, for some reason it's not possible to tab from ui.msgEdit to 'chat'
+//        for (int i = 0; i < layout->count() - 1; ++i)
+//            ui.chatTab->setTabOrder(layout->itemAt(i)->widget(), layout->itemAt(i+1)->widget());
         ui.chatTab->setTabOrder(chat, ui.msgEdit);
+        ui.chatTab->setTabOrder(ui.msgEdit, ui.sendButton);
         m_chathistory[TAB_CHAT] = chat;
         delete ui.chatEdit;
         ui.chatEdit = nullptr;
@@ -2787,6 +2791,7 @@ void MainWindow::setupChatHistory()
         layout->removeWidget(ui.videochatEdit);
         layout->insertWidget(index, video);
         ui.videoTab->setTabOrder(video, ui.videomsgEdit);
+        ui.videoTab->setTabOrder(ui.videomsgEdit, ui.videosendButton);
         m_chathistory[TAB_VIDEO] = video;
         delete ui.videochatEdit;
         ui.videochatEdit = nullptr;
@@ -2797,6 +2802,7 @@ void MainWindow::setupChatHistory()
         layout->removeWidget(ui.desktopchatEdit);
         layout->insertWidget(index, desktop);
         ui.desktopTab->setTabOrder(desktop, ui.desktopmsgEdit);
+        ui.desktopTab->setTabOrder(ui.desktopmsgEdit, ui.desktopsendButton);
         m_chathistory[TAB_DESKTOP] = desktop;
         delete ui.desktopchatEdit;
         ui.desktopchatEdit = nullptr;
