3a07d673c0ec5b0bb11ee0040c1fb165619f3f1a
Corentin BACQUÉ-CAZENAVE <corentin@progaccess.net>
2025-08-04 15:18:05 +0200

    Move exit steps to closeEvent

--- Client/qtTeamTalk/mainwindow.cpp
+++ Client/qtTeamTalk/mainwindow.cpp
@@ -4518,15 +4518,7 @@ void MainWindow::slotClientRecordConversations(bool/* checked*/)
 
 void MainWindow::slotClientExit(bool /*checked =false */)
 {
-    //close using timer, otherwise gets a Qt assertion from the 
-    //'setQuitOnLastWindowClosed' call.
-#if defined(ENABLE_TOLK)
-    if(Tolk_IsLoaded())
-        Tolk_Unload();
-#endif
-    if(TT_GetFlags(ttInst) & CLIENT_CONNECTED)
-        disconnectFromServer();
-    QApplication::quit();
+    this->close();
 }
 
 void MainWindow::slotMeChangeNickname(bool /*checked =false */)
@@ -8025,7 +8017,14 @@ void MainWindow::closeEvent(QCloseEvent *event)
 #if defined(Q_OS_DARWIN)
         QMainWindow::closeEvent(event);
 #else
-        slotClientExit();
+        //close using timer, otherwise gets a Qt assertion from the 
+        //'setQuitOnLastWindowClosed' call.
+#if defined(ENABLE_TOLK)
+        if(Tolk_IsLoaded())
+            Tolk_Unload();
+#endif
+        if(TT_GetFlags(ttInst) & CLIENT_CONNECTED)
+            disconnectFromServer();
 #endif
     }
     else
