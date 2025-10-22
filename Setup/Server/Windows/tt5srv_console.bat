80f0cd39a899295fd9ddbe406b6c21ce34c9e6a1
hwangsihu <129564966+hwangsihu@users.noreply.github.com>
2025-10-23 07:50:37 +0900

    Add Administrator privileges check

--- Setup/Server/Windows/tt5srv_console.bat
+++ Setup/Server/Windows/tt5srv_console.bat
@@ -1,8 +1,34 @@
 @echo off
+
+echo %~dp0 | findstr /I /C:"Program Files" >nul
+if %errorlevel% == 0 (
+    net session >nul 2>&1
+    if %errorlevel% == 0 (
+        set TTSRVCFG=%~dp0tt5srv.xml
+        set ADMIN_MODE=1
+    ) else (
+        echo Requesting administrator privileges...
+        >nul 2>&1 powershell -Command "Start-Process '%~f0' -Verb RunAs" && exit /b
+
+        set TTSRVCFG=%APPDATA%\BearWare.dk\tt5srv.xml
+        set ADMIN_MODE=0
+    )
+) else (
+    set TTSRVCFG=%~dp0tt5srv.xml
+    set ADMIN_MODE=0
+)
+
 echo   --------------------------------------------------------------------------
 echo                       TeamTalk 5 Console Server
 echo   --------------------------------------------------------------------------
 echo.
-tt5srv.exe -wizard
-tt5srv.exe -nd -verbose
+if "%ADMIN_MODE%"=="1" (
+    echo Running with administrator privileges
+) else (
+    echo Running without administrator privileges
+)
+echo Config file: %TTSRVCFG%
+echo.
+tt5srv.exe -wizard -c %TTSRVCFG%
+tt5srv.exe -nd -verbose -c %TTSRVCFG%
 pause
