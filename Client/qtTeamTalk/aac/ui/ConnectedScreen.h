976e7d7d986de07d20d656d8b743ec1d31fe2e58
trancer1994 <53197734+trancer1994@users.noreply.github.com>
2026-01-12 21:52:19 +0000

    Update ConnectedScreen.h

--- Client/qtTeamTalk/aac/ui/ConnectedScreen.h
+++ Client/qtTeamTalk/aac/ui/ConnectedScreen.h
@@ -5,6 +5,10 @@ class ConnectedScreen : public QWidget {
     Q_OBJECT
 public:
     explicit ConnectedScreen(QWidget* parent = nullptr);
+
+private:
+    QVBoxLayout* m_channelListLayout = nullptr;
+
 signals:
     void joinChannelRequested(int channelId);
 };
