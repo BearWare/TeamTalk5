64018f641312782cf2fde38f8552d1562bd759e5
trancer1994 <53197734+trancer1994@users.noreply.github.com>
2026-01-14 16:05:16 +0000

    Update MainWindow.h

--- Client/qtTeamTalk/aac/app/MainWindow.h
+++ Client/qtTeamTalk/aac/app/MainWindow.h
@@ -6,6 +6,12 @@
 class BackendAdapter;
 class StateMachine;
 
+// Forward declare your screen classes
+class ConnectScreen;
+class ConnectingScreen;
+class ChannelListScreen;
+class InChannelScreen;
+
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
@@ -16,10 +22,13 @@ public:
 private:
     BackendAdapter* m_backend = nullptr;
     StateMachine* m_stateMachine = nullptr;
-ConnectScreen* m_connectScreen = nullptr;
+
+    // AAC UI screens
+    ConnectScreen* m_connectScreen = nullptr;
+    ConnectingScreen* m_connectingScreen = nullptr;
+    ChannelListScreen* m_channelListScreen = nullptr;
+    InChannelScreen* m_inChannelScreen = nullptr;
 
 private slots:
-    void onConnectionStateChanged(ConnectionState state);
-    void onChannelEvent(const ChannelEvent& event);
     void onBackendError(const ErrorEvent& error);
 };
