f653899c1e7897905e42f6a70b270f3fa971ae0b
trancer1994 <53197734+trancer1994@users.noreply.github.com>
2026-01-14 14:55:06 +0000

    Update MainWindow.cpp

--- Client/qtTeamTalk/aac/app/MainWindow.cpp
+++ Client/qtTeamTalk/aac/app/MainWindow.cpp
@@ -1,28 +1,33 @@
 #include "MainWindow.h"
 #include "backend/BackendAdapter.h"
+#include "backend/StateMachine.h"
 
 #include <QMessageBox>
 
 MainWindow::MainWindow(QWidget* parent)
-    : QMainWindow(parent),
-      m_backend(new BackendAdapter(this))
+    : QMainWindow(parent)
 {
-    setupBackendConnections();
+    // Create backend + state machine
+    m_backend = new BackendAdapter(this);
+    m_stateMachine = new StateMachine(this);
+
+    // Wire backend → state machine
+    m_stateMachine->attachBackend(m_backend);
+
+    // Wire state machine → UI
+    connect(m_stateMachine, &StateMachine::connectionStateChanged,
+            this, &MainWindow::onConnectionStateChanged);
+
+    connect(m_stateMachine, &StateMachine::channelChanged,
+            this, &MainWindow::onChannelEvent);
+
+    connect(m_stateMachine, &StateMachine::errorOccurred,
+            this, &MainWindow::onBackendError);
+
+    // UI → state machine wiring will go here later
 }
 
 MainWindow::~MainWindow() = default;
-
-void MainWindow::setupBackendConnections()
-{
-    connect(m_backend, &BackendAdapter::connectionStateChanged,
-            this, &MainWindow::onConnectionStateChanged);
-
-    connect(m_backend, &BackendAdapter::channelEvent,
-            this, &MainWindow::onChannelEvent);
-
-    connect(m_backend, &BackendAdapter::errorOccurred,
-            this, &MainWindow::onBackendError);
-}
 
 void MainWindow::onConnectionStateChanged(ConnectionState state)
 {
