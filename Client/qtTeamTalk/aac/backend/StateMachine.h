86af194e29308e99ff403279dc2efb0d9c3b6f4d
trancer1994 <53197734+trancer1994@users.noreply.github.com>
2026-01-14 22:07:54 +0000

    Update StateMachine.h

--- Client/qtTeamTalk/aac/backend/StateMachine.h
+++ Client/qtTeamTalk/aac/backend/StateMachine.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <QObject>
+#include <QTimer>
 #include "BackendEvents.h"
 #include "BackendAdapter.h"
 #include "BackendState.h"
@@ -9,15 +10,20 @@ class StateMachine : public QObject {
     Q_OBJECT
 public:
     explicit StateMachine(QObject* parent = nullptr);
+    void stopAutoReconnect();
 
     void attachBackend(BackendAdapter* backend);
 
 signals:
     // UI-facing signals
     void channelListChanged(const QList<QPair<int, QString>>& channels);
-void connectionStateChanged(ConnectionState state);
+    void connectionStateChanged(ConnectionState state);
     void channelChanged(int channelId);
     void errorOccurred(const QString& message);
+
+    void notifyUser(const QString& message);
+    void reconnecting(int attempt, int delayMs);
+    void reconnectStopped();
 
     void selfVoiceStateChanged(SelfVoiceState state);
     void audioDeviceStateChanged(AudioDeviceEventType type);
@@ -33,15 +39,23 @@ public slots:
 
 private slots:
     void onConnectionStateChanged(ConnectionState state);
+    void attemptReconnect();
     void onChannelEvent(const ChannelEvent& event);
     void onErrorOccurred(const ErrorEvent& error);
     void onChannelsEnumerated(const QList<ChannelInfo>& channels);
 
-void onSelfVoiceEvent(const SelfVoiceEvent& event);
+    void onSelfVoiceEvent(const SelfVoiceEvent& event);
     void onAudioDeviceEvent(const AudioDeviceEvent& event);
     void onTextMessageEvent(const TextMessageEvent& event);
 
 private:
+    QTimer* m_reconnectTimer = nullptr;
+    int m_reconnectAttempts = 0;
+    int m_maxReconnectAttempts = 10;
+
+    QString m_lastHost;
+    int m_lastPort = 0;
+
     BackendAdapter* m_backend = nullptr;
     BackendState m_state;
 };
