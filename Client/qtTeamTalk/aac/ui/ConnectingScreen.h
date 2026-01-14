f5a02360129ee0295d4c8f4b769b8ef001bdbac1
trancer1994 <53197734+trancer1994@users.noreply.github.com>
2026-01-14 18:43:18 +0000

    Update ConnectingScreen.h

--- Client/qtTeamTalk/aac/ui/ConnectingScreen.h
+++ Client/qtTeamTalk/aac/ui/ConnectingScreen.h
@@ -1,8 +1,15 @@
 #pragma once
+
 #include <QWidget>
 
-class ConnectingScreen : public QWidget {
+class QLabel;
+
+class ConnectingScreen : public QWidget
+{
     Q_OBJECT
 public:
     explicit ConnectingScreen(QWidget* parent = nullptr);
+
+private:
+    QLabel* m_label = nullptr;
 };
