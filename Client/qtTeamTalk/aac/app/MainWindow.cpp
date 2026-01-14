da697059d54068c6ed047cd42eb165db1352c621
trancer1994 <53197734+trancer1994@users.noreply.github.com>
2026-01-14 16:01:09 +0000

    Update MainWindow.cpp

--- Client/qtTeamTalk/aac/app/MainWindow.cpp
+++ Client/qtTeamTalk/aac/app/MainWindow.cpp
@@ -10,54 +10,47 @@ MainWindow::MainWindow(QWidget* parent)
     // Create backend + state machine
     m_backend = new BackendAdapter(this);
     m_stateMachine = new StateMachine(this);
-showScreen(m_connectScreen);
 
-m_connectScreen = new ConnectScreen(this);
-m_connectingScreen = new ConnectingScreen(this);
-m_channelListScreen = new ChannelListScreen(this);
-m_inChannelScreen = new InChannelScreen(this);
+    // Create screens (must happen BEFORE showScreen)
+    m_connectScreen = new ConnectScreen(this);
+    m_connectingScreen = new ConnectingScreen(this);
+    m_channelListScreen = new ChannelListScreen(this);
+    m_inChannelScreen = new InChannelScreen(this);
+
     // Wire backend → state machine
     m_stateMachine->attachBackend(m_backend);
 
     // Wire state machine → UI
-connect(m_stateMachine, &StateMachine::connectionStateChanged,
-        this, &MainWindow::updateScreenForConnectionState);
+    connect(m_stateMachine, &StateMachine::connectionStateChanged,
+            this, &MainWindow::updateScreenForConnectionState);
 
-connect(m_stateMachine, &StateMachine::channelChanged,
-        this, &MainWindow::updateScreenForChannelState);
+    connect(m_stateMachine, &StateMachine::channelChanged,
+            this, &MainWindow::updateScreenForChannelState);
 
     connect(m_stateMachine, &StateMachine::errorOccurred,
             this, &MainWindow::onBackendError);
 
-// UI → state machine wiring
+    // UI → state machine wiring
+    connect(m_connectScreen, &ConnectScreen::connectRequested,
+            m_stateMachine, &StateMachine::onConnectRequested);
 
-connect(m_connectScreen, &ConnectScreen::connectRequested,
-        m_stateMachine, &StateMachine::onConnectRequested);
+    connect(m_channelListScreen, &ChannelListScreen::refreshRequested,
+            m_stateMachine, &StateMachine::onRefreshChannelsRequested);
 
-connect(m_channelListScreen, &ChannelListScreen::refreshRequested,
-        m_stateMachine, &StateMachine::onRefreshChannelsRequested);
+    connect(m_channelListScreen, &ChannelListScreen::joinChannelRequested,
+            m_stateMachine, &StateMachine::onJoinChannelRequested);
 
-connect(m_channelListScreen, &ChannelListScreen::joinChannelRequested,
-        m_stateMachine, &StateMachine::onJoinChannelRequested);
+    connect(m_inChannelScreen, &InChannelScreen::leaveRequested,
+            m_stateMachine, &StateMachine::onLeaveChannelRequested);
 
-connect(m_inChannelScreen, &InChannelScreen::leaveRequested,
-        m_stateMachine, &StateMachine::onLeaveChannelRequested);
+    connect(m_inChannelScreen, &InChannelScreen::transmitToggled,
+            m_stateMachine, &StateMachine::onTransmitToggled);
 
-connect(m_inChannelScreen, &InChannelScreen::transmitToggled,
-        m_stateMachine, &StateMachine::onTransmitToggled);
+    // Start on the Connect screen
+    showScreen(m_connectScreen);
 }
 
 MainWindow::~MainWindow() = default;
-
-void MainWindow::onConnectionStateChanged(ConnectionState state)
-{
-    updateScreenForConnectionState(state);
-}
-
-void MainWindow::onChannelEvent(const ChannelEvent& event)
-{
-    updateScreenForChannelState(event.channelId);
-}
 
 void MainWindow::onBackendError(const ErrorEvent& error)
 {
