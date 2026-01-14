1420026630c308fad742d6bff8dc477a06c14665
trancer1994 <53197734+trancer1994@users.noreply.github.com>
2026-01-14 18:49:31 +0000

    Update InChannelScreen.cpp

--- Client/qtTeamTalk/aac/ui/InChannelScreen.cpp
+++ Client/qtTeamTalk/aac/ui/InChannelScreen.cpp
@@ -1,26 +1,28 @@
 #include "InChannelScreen.h"
 #include <QVBoxLayout>
 #include <QPushButton>
-#include <QLabel>
 
 InChannelScreen::InChannelScreen(QWidget* parent)
     : QWidget(parent)
 {
-    auto layout = new QVBoxLayout(this);
+    auto* layout = new QVBoxLayout(this);
 
-    m_transmitBtn = new QPushButton("Not Speaking", this);
-    m_transmitBtn->setCheckable(true);
+    m_transmitButton = new QPushButton("Start Talking", this);
+    m_transmitButton->setMinimumHeight(80);
 
-    layout->addWidget(m_transmitBtn);
-}
+    m_leaveButton = new QPushButton("Leave Channel", this);
+    m_leaveButton->setMinimumHeight(80);
 
-void InChannelScreen::setTransmitState(bool active)
-{
-    m_transmitBtn->setChecked(active);
-    m_transmitBtn->setText(active ? "Speaking" : "Not Speaking");
-}
+    layout->addWidget(m_transmitButton);
+    layout->addWidget(m_leaveButton);
+    layout->addStretch();
 
-void InChannelScreen::showMessageBubble(int fromUserId, const QString& message)
-{
-    // TODO: add message bubble widget
+    connect(m_transmitButton, &QPushButton::clicked, this, [this]() {
+        m_transmitting = !m_transmitting;
+        m_transmitButton->setText(m_transmitting ? "Stop Talking" : "Start Talking");
+        emit transmitToggled(m_transmitting);
+    });
+
+    connect(m_leaveButton, &QPushButton::clicked,
+            this, &InChannelScreen::leaveRequested);
 }
