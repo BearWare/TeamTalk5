259d96804d463f0f4efb9f58e183a527a4851b9e
trancer1994 <53197734+trancer1994@users.noreply.github.com>
2026-01-14 15:33:12 +0000

    Update MainWindow.cpp

--- Client/qtTeamTalk/aac/app/MainWindow.cpp
+++ Client/qtTeamTalk/aac/app/MainWindow.cpp
@@ -10,36 +10,89 @@ MainWindow::MainWindow(QWidget* parent)
     // Create backend + state machine
     m_backend = new BackendAdapter(this);
     m_stateMachine = new StateMachine(this);
+showScreen(m_connectScreen);
 
+m_connectScreen = new ConnectScreen(this);
+m_connectingScreen = new ConnectingScreen(this);
+m_channelListScreen = new ChannelListScreen(this);
+m_inChannelScreen = new InChannelScreen(this);
     // Wire backend → state machine
     m_stateMachine->attachBackend(m_backend);
 
     // Wire state machine → UI
-    connect(m_stateMachine, &StateMachine::connectionStateChanged,
-            this, &MainWindow::onConnectionStateChanged);
+connect(m_stateMachine, &StateMachine::connectionStateChanged,
+        this, &MainWindow::updateScreenForConnectionState);
 
-    connect(m_stateMachine, &StateMachine::channelChanged,
-            this, &MainWindow::onChannelEvent);
+connect(m_stateMachine, &StateMachine::channelChanged,
+        this, &MainWindow::updateScreenForChannelState);
 
     connect(m_stateMachine, &StateMachine::errorOccurred,
             this, &MainWindow::onBackendError);
 
-    // UI → state machine wiring will go here later
+// UI → state machine wiring
+
+connect(m_connectScreen, &ConnectScreen::connectRequested,
+        m_stateMachine, &StateMachine::onConnectRequested);
+
+connect(m_channelListScreen, &ChannelListScreen::refreshRequested,
+        m_stateMachine, &StateMachine::onRefreshChannelsRequested);
+
+connect(m_channelListScreen, &ChannelListScreen::joinChannelRequested,
+        m_stateMachine, &StateMachine::onJoinChannelRequested);
+
+connect(m_inChannelScreen, &InChannelScreen::leaveRequested,
+        m_stateMachine, &StateMachine::onLeaveChannelRequested);
+
+connect(m_inChannelScreen, &InChannelScreen::transmitToggled,
+        m_stateMachine, &StateMachine::onTransmitToggled);
 }
 
 MainWindow::~MainWindow() = default;
 
 void MainWindow::onConnectionStateChanged(ConnectionState state)
 {
-    // TODO: update AAC-native UI to reflect connection state
+    updateScreenForConnectionState(state);
 }
 
 void MainWindow::onChannelEvent(const ChannelEvent& event)
 {
-    // TODO: update AAC-native UI to reflect channel join/leave
+    updateScreenForChannelState(event.channelId);
 }
 
 void MainWindow::onBackendError(const ErrorEvent& error)
 {
     QMessageBox::warning(this, tr("Error"), error.message);
+}
+void MainWindow::showScreen(QWidget* screen)
+{
+    setCentralWidget(screen);
+    screen->show();
+}
+
+void MainWindow::updateScreenForConnectionState(ConnectionState state)
+{
+    switch (state) {
+    case ConnectionState::Disconnected:
+        showScreen(m_connectScreen);
+        break;
+
+    case ConnectionState::Connecting:
+        showScreen(m_connectingScreen);
+        break;
+
+    case ConnectionState::Connected:
+        showScreen(m_channelListScreen);
+        break;
+    }
+}
+
+void MainWindow::updateScreenForChannelState(int channelId)
+{
+    if (channelId == -1) {
+        // Not in a channel → show channel list
+        showScreen(m_channelListScreen);
+    } else {
+        // In a channel → show in-channel screen
+        showScreen(m_inChannelScreen);
+    }
 }
