97a0f314d68bf9c6f282342e62505212703b3f82
trancer1994 <53197734+trancer1994@users.noreply.github.com>
2026-01-15 13:10:52 +0000

    Update ConnectScreen.h

--- Client/qtTeamTalk/aac/ui/ConnectScreen.h
+++ Client/qtTeamTalk/aac/ui/ConnectScreen.h
@@ -6,17 +6,18 @@ class QLineEdit;
 class QSpinBox;
 class QPushButton;
 
-class ConnectScreen : public QWidget
-{
+class ConnectScreen : public QWidget {
     Q_OBJECT
 public:
     explicit ConnectScreen(QWidget* parent = nullptr);
 
 signals:
-    void connectRequested(const QString& host, int port);
+    void connectRequested();
+    void connectToServer(const QString& host, int port);
 
 private:
-    QLineEdit* m_hostEdit = nullptr;
-    QSpinBox* m_portSpin = nullptr;
-    QPushButton* m_connectButton = nullptr;
+    QLineEdit* m_hostEdit;
+    QSpinBox* m_portEdit;
+    QPushButton* m_connectButton;
+    QPushButton* m_startButton;
 };
