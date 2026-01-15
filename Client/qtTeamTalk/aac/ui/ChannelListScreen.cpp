bb4c96c73136cdc771408787b63809aefb463ef3
trancer1994 <53197734+trancer1994@users.noreply.github.com>
2026-01-15 13:17:19 +0000

    Update ChannelListScreen.cpp

--- Client/qtTeamTalk/aac/ui/ChannelListScreen.cpp
+++ Client/qtTeamTalk/aac/ui/ChannelListScreen.cpp
@@ -9,34 +9,26 @@ ChannelListScreen::ChannelListScreen(QWidget* parent)
     auto* layout = new QVBoxLayout(this);
 
     m_list = new QListWidget(this);
-
-    m_refreshButton = new QPushButton("Refresh Channels", this);
-    m_refreshButton->setMinimumHeight(60);
-
-    m_joinButton = new QPushButton("Join Selected Channel", this);
-    m_joinButton->setMinimumHeight(60);
+    m_refreshButton = new QPushButton("Refresh", this);
 
     layout->addWidget(m_list);
     layout->addWidget(m_refreshButton);
-    layout->addWidget(m_joinButton);
 
     connect(m_refreshButton, &QPushButton::clicked,
             this, &ChannelListScreen::refreshRequested);
 
-    connect(m_joinButton, &QPushButton::clicked, this, [this]() {
-        auto* item = m_list->currentItem();
-        if (!item) return;
-        int channelId = item->data(Qt::UserRole).toInt();
-        emit joinChannelRequested(channelId);
-    });
+    connect(m_list, &QListWidget::itemActivated,
+            this, [this](QListWidgetItem* item) {
+                emit joinChannelRequested(item->data(Qt::UserRole).toInt());
+            });
 }
 
-void ChannelListScreen::setChannels(const QList<QPair<int, QString>>& channels)
+void ChannelListScreen::setChannels(const QList<ChannelInfo>& channels)
 {
     m_list->clear();
-    for (const auto& pair : channels) {
-        auto* item = new QListWidgetItem(pair.second);
-        item->setData(Qt::UserRole, pair.first);
-        m_list->addItem(item);
+
+    for (const ChannelInfo& ci : channels) {
+        auto* item = new QListWidgetItem(ci.name, m_list);
+        item->setData(Qt::UserRole, ci.id);
     }
 }
