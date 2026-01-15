c3a815a88aa6fa08d2cd289fcd778d4f0bb7bc9a
trancer1994 <53197734+trancer1994@users.noreply.github.com>
2026-01-15 13:20:34 +0000

    Update InChannelScreen.cpp

--- Client/qtTeamTalk/aac/ui/InChannelScreen.cpp
+++ Client/qtTeamTalk/aac/ui/InChannelScreen.cpp
@@ -1,28 +1,35 @@
 #include "InChannelScreen.h"
 #include <QVBoxLayout>
 #include <QPushButton>
+#include <QLabel>
 
 InChannelScreen::InChannelScreen(QWidget* parent)
     : QWidget(parent)
 {
     auto* layout = new QVBoxLayout(this);
 
-    m_transmitButton = new QPushButton("Start Talking", this);
-    m_transmitButton->setMinimumHeight(80);
+    m_transmitButton = new QPushButton("Transmit", this);
+    m_transmitButton->setCheckable(true);
 
     m_leaveButton = new QPushButton("Leave Channel", this);
-    m_leaveButton->setMinimumHeight(80);
+
+    m_voiceIndicator = new QLabel("Silent", this);
 
     layout->addWidget(m_transmitButton);
     layout->addWidget(m_leaveButton);
-    layout->addStretch();
+    layout->addWidget(m_voiceIndicator);
 
-    connect(m_transmitButton, &QPushButton::clicked, this, [this]() {
-        m_transmitting = !m_transmitting;
-        m_transmitButton->setText(m_transmitting ? "Stop Talking" : "Start Talking");
-        emit transmitToggled(m_transmitting);
-    });
+    connect(m_transmitButton, &QPushButton::toggled,
+            this, &InChannelScreen::transmitToggled);
 
     connect(m_leaveButton, &QPushButton::clicked,
             this, &InChannelScreen::leaveRequested);
+}
+
+void InChannelScreen::setSelfVoiceState(SelfVoiceState state)
+{
+    if (state == SelfVoiceState::Talking)
+        m_voiceIndicator->setText("Talking");
+    else
+        m_voiceIndicator->setText("Silent");
 }
