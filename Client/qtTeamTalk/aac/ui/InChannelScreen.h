ed83975c0a14b5556a1dc6e44f640b9cf372efc6
trancer1994 <53197734+trancer1994@users.noreply.github.com>
2026-01-15 13:19:20 +0000

    Update InChannelScreen.h

--- Client/qtTeamTalk/aac/ui/InChannelScreen.h
+++ Client/qtTeamTalk/aac/ui/InChannelScreen.h
@@ -1,21 +1,25 @@
 #pragma once
 
 #include <QWidget>
+#include "../backend/BackendEvents.h"
 
 class QPushButton;
+class QLabel;
 
-class InChannelScreen : public QWidget
-{
+class InChannelScreen : public QWidget {
     Q_OBJECT
 public:
     explicit InChannelScreen(QWidget* parent = nullptr);
 
+public slots:
+    void setSelfVoiceState(SelfVoiceState state);
+
 signals:
+    void transmitToggled(bool enabled);
     void leaveRequested();
-    void transmitToggled(bool on);
 
 private:
-    QPushButton* m_leaveButton = nullptr;
-    QPushButton* m_transmitButton = nullptr;
-    bool m_transmitting = false;
+    QPushButton* m_transmitButton;
+    QPushButton* m_leaveButton;
+    QLabel* m_voiceIndicator;
 };
