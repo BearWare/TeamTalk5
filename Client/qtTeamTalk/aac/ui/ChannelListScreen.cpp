f6c041e5de2631c09306b74140ab22dfb77f0ccb
trancer1994 <53197734+trancer1994@users.noreply.github.com>
2026-01-16 23:49:12 +0000

    Update ChannelListScreen.cpp

--- Client/qtTeamTalk/aac/ui/ChannelListScreen.cpp
+++ Client/qtTeamTalk/aac/ui/ChannelListScreen.cpp
@@ -11,16 +11,30 @@ ChannelListScreen::ChannelListScreen(AACAccessibilityManager* aac, QWidget* pare
     auto* layout = new QVBoxLayout(this);
 
     m_list = new QListWidget(this);
-    m_refreshButton = new QPushButton("Refresh", this);
-
     layout->addWidget(m_list);
-    layout->addWidget(m_refreshButton);
-
     registerInteractive(m_list);
+
+    m_refreshButton = new QPushButton(tr("Refresh"), this);
+    layout->addWidget(m_refreshButton);
     registerInteractive(m_refreshButton, true);
+
+    // New: Settings + AAC Access buttons
+    auto* settingsButton = new QPushButton(tr("Settings"), this);
+    layout->addWidget(settingsButton);
+    registerInteractive(settingsButton);
+
+    auto* aacButton = new QPushButton(tr("AAC Access"), this);
+    layout->addWidget(aacButton);
+    registerInteractive(aacButton);
 
     connect(m_refreshButton, &QPushButton::clicked,
             this, &ChannelListScreen::refreshRequested);
+
+    connect(settingsButton, &QPushButton::clicked,
+            this, &ChannelListScreen::settingsRequested);
+
+    connect(aacButton, &QPushButton::clicked,
+            this, &ChannelListScreen::aacSettingsRequested);
 
     connect(m_list, &QListWidget::itemActivated,
             this, [this](QListWidgetItem* item) {
