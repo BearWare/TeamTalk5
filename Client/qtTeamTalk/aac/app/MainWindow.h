f04052d20e9672c51e00df55a2fca5f8490e2751
trancer1994 <53197734+trancer1994@users.noreply.github.com>
2026-01-16 20:41:59 +0000

    Update MainWindow.h

--- Client/qtTeamTalk/aac/app/MainWindow.h
+++ Client/qtTeamTalk/aac/app/MainWindow.h
@@ -1,12 +1,11 @@
 #pragma once
 
 #include <QMainWindow>
-#include <QList>
 
 class BackendAdapter;
 class StateMachine;
 
-class AACScreen;
+class AACAccessibilityManager;
 class ConnectScreen;
 class ConnectingScreen;
 class ChannelListScreen;
@@ -19,6 +18,9 @@ public:
     ~MainWindow();
 
 private:
+    // AAC framework
+    AACAccessibilityManager* m_aac = nullptr;
+
     // Backend + StateMachine
     BackendAdapter* m_backend = nullptr;
     StateMachine* m_stateMachine = nullptr;
@@ -29,19 +31,14 @@ private:
     ChannelListScreen* m_channelListScreen = nullptr;
     InChannelScreen* m_inChannelScreen = nullptr;
 
-    // All AAC screens for Large‑Target Mode propagation
-    QList<AACScreen*> m_aacScreens;
-
-    // Helpers
     void showScreen(QWidget* screen);
 
 private slots:
-    // StateMachine → UI
     void showConnectScreen();
     void showConnectingScreen();
     void showChannelListScreen();
     void showInChannelScreen();
 
-    // Large‑Target Mode → UI
-    void onLargeTargetModeChanged(bool enabled);
+protected:
+    void keyPressEvent(QKeyEvent* e) override;
 };
