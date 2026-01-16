034ea08a7541bcc9dd888460d62b6bf0f792a1e1
trancer1994 <53197734+trancer1994@users.noreply.github.com>
2026-01-16 23:55:44 +0000

    Update ConnectingScreen.h

--- Client/qtTeamTalk/aac/ui/ConnectingScreen.h
+++ Client/qtTeamTalk/aac/ui/ConnectingScreen.h
@@ -4,6 +4,10 @@
 
 class QPushButton;
 
+// Simple connecting screen:
+// - “Connecting…” label
+// - Cancel button
+// - No settings buttons (per Option A1)
 class ConnectingScreen : public AACScreen {
     Q_OBJECT
 public:
