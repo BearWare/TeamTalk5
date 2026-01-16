23459119b50a89539c8685e8129f772b7cc0af4b
trancer1994 <53197734+trancer1994@users.noreply.github.com>
2026-01-16 23:41:39 +0000

    Update ConnectScreen.h

--- Client/qtTeamTalk/aac/ui/ConnectScreen.h
+++ Client/qtTeamTalk/aac/ui/ConnectScreen.h
@@ -6,6 +6,14 @@ class QLineEdit;
 class QSpinBox;
 class QPushButton;
 
+// Connect screen with:
+// - Host
+// - Port
+// - Username
+// - Connect button
+// - Error line
+// - Settings (App Settings)
+// - AAC Access (AAC Settings)
 class ConnectScreen : public AACScreen {
     Q_OBJECT
 public:
@@ -15,11 +23,13 @@ public:
 
 signals:
     void connectRequested(const QString& host, int port, const QString& username);
+    void settingsRequested();
+    void aacSettingsRequested();
 
 private:
-    QLineEdit* m_hostEdit = nullptr;
-    QSpinBox* m_portEdit = nullptr;
-    QLineEdit* m_usernameEdit = nullptr;
+    QLineEdit*  m_hostEdit     = nullptr;
+    QSpinBox*   m_portEdit     = nullptr;
+    QLineEdit*  m_usernameEdit = nullptr;
     QPushButton* m_connectButton = nullptr;
-    QLineEdit* m_errorLine = nullptr;
+    QLineEdit*  m_errorLine    = nullptr;
 };
