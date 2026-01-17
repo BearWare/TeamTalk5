ebaac0206eeadc6d06980d44d4f69d6e7eaf714f
trancer1994 <53197734+trancer1994@users.noreply.github.com>
2026-01-17 22:34:46 +0000

    Add AAC Predictive strip widget header for UI

---
+++ Client/qtTeamTalk/aac/ui/AACPredictiveStrip.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <QWidget>
+#include <QStringList>
+
+class AACAccessibilityManager;
+
+class AACPredictiveStrip : public QWidget {
+    Q_OBJECT
+public:
+    explicit AACPredictiveStrip(AACAccessibilityManager* aac, QWidget* parent = nullptr);
+
+    void setSuggestions(const QStringList& words);
+
+signals:
+    void suggestionActivated(const QString& word);
+
+protected:
+    void paintEvent(QPaintEvent* e) override;
+    void mousePressEvent(QMouseEvent* e) override;
+
+private:
+    AACAccessibilityManager* m_aac = nullptr;
+    QStringList m_words;
+};
