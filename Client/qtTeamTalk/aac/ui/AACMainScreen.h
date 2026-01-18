732237a59fea9e27a9c7c1ed5129921dad1eea92
trancer1994 <53197734+trancer1994@users.noreply.github.com>
2026-01-18 19:45:27 +0000

    Create AACMainScreen.h

---
+++ Client/qtTeamTalk/aac/ui/AACMainScreen.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <QWidget>
+#include <QPointer>
+
+class QStackedWidget;
+class QPushButton;
+class AACAccessibilityManager;
+class AACTextBar;
+class AACKeyboardScreen;
+class AACSymbolGridScreen;
+class AACCategoryScreen;
+class PredictiveStrip; // your existing widget
+
+class AACMainScreen : public QWidget {
+    Q_OBJECT
+public:
+    explicit AACMainScreen(AACAccessibilityManager* mgr, QWidget* parent = nullptr);
+
+private:
+    void buildUi();
+    void connectSignals();
+    void switchToKeyboard();
+    void switchToSymbols();
+    void switchToCategories();
+
+    AACAccessibilityManager* m_mgr = nullptr;
+
+    AACTextBar* m_textBar = nullptr;
+    PredictiveStrip* m_predictive = nullptr;
+
+    QStackedWidget* m_stack = nullptr;
+
+    AACKeyboardScreen* m_keyboardScreen = nullptr;
+    AACSymbolGridScreen* m_symbolScreen = nullptr;
+    AACCategoryScreen* m_categoryScreen = nullptr;
+
+    QPushButton* m_keyboardBtn = nullptr;
+    QPushButton* m_symbolsBtn = nullptr;
+    QPushButton* m_categoriesBtn = nullptr;
+};
