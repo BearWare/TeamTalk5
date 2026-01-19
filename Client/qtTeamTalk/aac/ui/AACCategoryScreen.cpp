716b220a7af7882bd8e63b24f33142a82d7794a8
trancer1994 <53197734+trancer1994@users.noreply.github.com>
2026-01-19 13:07:56 +0000

    Update AACCategoryScreen.cpp

--- Client/qtTeamTalk/aac/ui/AACCategoryScreen.cpp
+++ Client/qtTeamTalk/aac/ui/AACCategoryScreen.cpp
@@ -1,22 +1,45 @@
 #include "AACCategoryScreen.h"
 #include "AACCategoryButton.h"
+#include "AACVocabularyManager.h"
+
 #include <QGridLayout>
 
-AACCategoryScreen::AACCategoryScreen(AACAccessibilityManager* mgr, QWidget* parent)
-    : AACScreen(mgr, parent)
+AACCategoryScreen::AACCategoryScreen(AACAccessibilityManager* mgr,
+                                     AACVocabularyManager* vocab,
+                                     QWidget* parent)
+    : AACScreen(mgr, parent),
+      m_vocab(vocab)
 {
     m_grid = new QGridLayout(this);
     setLayout(m_grid);
+
     buildCategories();
+
+    // React to live updates
+    connect(m_vocab, &AACVocabularyManager::categoriesChanged,
+            this, &AACCategoryScreen::buildCategories);
+}
+
+void AACCategoryScreen::clearCategories()
+{
+    QLayoutItem* item;
+    while ((item = m_grid->takeAt(0)) != nullptr) {
+        if (item->widget())
+            delete item->widget();
+        delete item;
+    }
 }
 
 void AACCategoryScreen::buildCategories()
 {
-    QStringList cats = {"People", "Food", "Places", "Actions", "Feelings", "Things"};
+    clearCategories();
+
+    QStringList cats = m_vocab->categories();
 
     int row = 0, col = 0;
     for (const QString& c : cats) {
         AACCategoryButton* btn = new AACCategoryButton(c, m_aac, this);
+
         m_grid->addWidget(btn, row, col++);
         registerInteractive(btn, true);
 
