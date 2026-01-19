cd91cc884806d9b0e34f2f18feebe92cbf499114
trancer1994 <53197734+trancer1994@users.noreply.github.com>
2026-01-19 13:04:43 +0000

    Update AACCategoryScreen.h

--- Client/qtTeamTalk/aac/ui/AACCategoryScreen.h
+++ Client/qtTeamTalk/aac/ui/AACCategoryScreen.h
@@ -5,13 +5,19 @@
 class QGridLayout;
 class AACCategoryButton;
 
+class AACVocabularyManager;
+
 class AACCategoryScreen : public AACScreen {
     Q_OBJECT
 public:
-    explicit AACCategoryScreen(AACAccessibilityManager* mgr, QWidget* parent = nullptr);
+    explicit AACCategoryScreen(AACAccessibilityManager* mgr,
+                               AACVocabularyManager* vocab,
+                               QWidget* parent = nullptr);
 
 private:
     void buildCategories();
+    void clearCategories();
 
     QGridLayout* m_grid = nullptr;
+    AACVocabularyManager* m_vocab = nullptr;
 };
