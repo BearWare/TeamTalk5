b09ec97438a178b70f482657eacf0b5be261d582
trancer1994 <53197734+trancer1994@users.noreply.github.com>
2026-01-24 17:26:13 +0000

    Update AACMainScreen.h

--- Client/qtTeamTalk/aac/ui/AACMainScreen.h
+++ Client/qtTeamTalk/aac/ui/AACMainScreen.h
@@ -1,45 +1,66 @@
 #pragma once
 
 #include <QWidget>
-#include <QPointer>
+#include <QListWidget>
+#include <QLineEdit>
+#include <QVBoxLayout>
+#include <QHBoxLayout>
+#include <QTimer>
+#include <QString>
+#include <QRegularExpression>
 
-class QStackedWidget;
-class QPushButton;
-class AACAccessibilityManager;
-class AACTextBar;
-class AACKeyboardScreen;
-class AACSymbolGridScreen;
-class AACCategoryScreen;
-class AACVocabularyManager;
-class PredictiveStrip;
+#include "AACFramework.h" // for AACScreenAdapter, AACAccessibilityManager, AACPredictionEngine
 
-class AACMainScreen : public QWidget {
+class QLabel;
+
+/**
+ * AACMainScreen
+ *
+ * Main AAC text composition screen:
+ * - Text input buffer
+ * - Prediction bar
+ * - Hooks into AACPredictionEngine via AACAccessibilityManager
+ * - Implements AACScreenAdapter for layout + scanning
+ */
+class AACMainScreen : public QWidget, public AACScreenAdapter
+{
     Q_OBJECT
 public:
-    explicit AACMainScreen(AACAccessibilityManager* mgr,
-                           AACVocabularyManager* vocab,
-                           QWidget* parent = nullptr);
+    explicit AACMainScreen(AACAccessibilityManager* mgr, QWidget* parent = nullptr);
+
+    // AACScreenAdapter implementation
+    QList<QWidget*> interactiveWidgets() const override;
+    QList<QWidget*> primaryWidgets() const override;
+    QLayout* rootLayout() const override;
+    QWidget* predictiveStripContainer() const override;
+
+signals:
+    void textCommitted(const QString& text);
+
+public slots:
+    void setText(const QString& text);
+
+private slots:
+    void onTextChanged(const QString& text);
+    void onPredictionClicked(QListWidgetItem* item);
+    void onCommitButtonClicked();
+    void onDeleteButtonClicked();
 
 private:
-    void buildUi();
-    void connectSignals();
-    void switchToKeyboard();
-    void switchToSymbols();
-    void switchToCategories();
+    QString extractPrevWord(const QString& text) const;
+    QString extractSecondLastWord(const QString& text) const;
 
     AACAccessibilityManager* m_mgr = nullptr;
-    AACVocabularyManager* m_vocab = nullptr;
+    AACPredictionEngine*     m_pred = nullptr;
 
-    AACTextBar* m_textBar = nullptr;
-    PredictiveStrip* m_predictive = nullptr;
+    QVBoxLayout*   m_rootLayout = nullptr;
+    QWidget*       m_predictiveStripContainer = nullptr;
 
-    QStackedWidget* m_stack = nullptr;
+    QLabel*        m_label = nullptr;
+    QLineEdit*     m_text = nullptr;
+    QListWidget*   m_predictionBar = nullptr;
+    QPushButton*   m_commitButton = nullptr;
+    QPushButton*   m_deleteButton = nullptr;
 
-    AACKeyboardScreen* m_keyboardScreen = nullptr;
-    AACSymbolGridScreen* m_symbolScreen = nullptr;
-    AACCategoryScreen* m_categoryScreen = nullptr;
-
-    QPushButton* m_keyboardBtn = nullptr;
-    QPushButton* m_symbolsBtn = nullptr;
-    QPushButton* m_categoriesBtn = nullptr;
+    QTimer         m_tickTimer;
 };
