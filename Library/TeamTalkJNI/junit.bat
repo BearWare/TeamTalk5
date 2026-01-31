8f35e571b04c2695e87331a4554e8bc4fbc7a247
Bjørn Damstedt Rasmussen <contact@bearware.dk>
2026-01-31 19:51:24 +0100

    Update .bat file for JUnit tests from Windows bat file

--- Library/TeamTalkJNI/junit.bat
+++ Library/TeamTalkJNI/junit.bat
@@ -1,29 +1,32 @@
 set TEAMTALKJNI_HOME=%CD%
 set PATH=%PATH%;%TEAMTALKJNI_HOME%\..\TeamTalk_DLL;%TEAMTALKJNI_HOME%\libs;
-set CLASSPATH=%TEAMTALKJNI_HOME%\libs\TeamTalk5.jar;%TEAMTALKJNI_HOME%\libs\junit4.jar;%TEAMTALKJNI_HOME%\libs\hamcrest-core.jar;
-
-cd %TEAMTALKJNI_HOME%\test
-javac.exe -source 1.7 -target 1.7 -g -cp %CLASSPATH% -sourcepath %TEAMTALKJNI_HOME%/src dk/bearware/TeamTalkStdTestCase.java dk/bearware/TeamTalkTestCase.java dk/bearware/TeamTalkTestCaseBase.java TeamTalkStdTestSuite.java SingleJUnitTestRunner.java
-jar.exe cf ../libs/TeamTalk5Test.jar ./dk/bearware/*.class
+set CLASSPATH=%TEAMTALKJNI_HOME%\libs\junit4.jar;%TEAMTALKJNI_HOME%\libs\hamcrest-core.jar
 
 @set JAVAPROP=-Djava.library.path=%TEAMTALKJNI_HOME%\libs
-@set JAVAPROP=%JAVAPROP% -Ddk.bearware.serverip=
-@set JAVAPROP=%JAVAPROP% -Ddk.bearware.sndinputid=
-@set JAVAPROP=%JAVAPROP% -Ddk.bearware.sndoutputid=
-@set JAVAPROP=%JAVAPROP% -Ddk.bearware.encrypted=0
-@set JAVAPROP=%JAVAPROP% -Ddk.bearware.videodevid=
-@set JAVAPROP=%JAVAPROP% -Ddk.bearware.serverip=127.0.0.1
-@set JAVAPROP=%JAVAPROP% -Ddk.bearware.serverbindip=
-@set JAVAPROP=%JAVAPROP% -Ddk.bearware.videofile=
-@set JAVAPROP=%JAVAPROP% -Ddk.bearware.audiofile=
-@set JAVAPROP=%JAVAPROP% -Ddk.bearware.httpsfile=
-@set JAVAPROP=%JAVAPROP% -Ddk.bearware.opustools=
-@set JAVAPROP=%JAVAPROP% -Ddk.bearware.verbose=0
-
-java.exe -cp %CLASSPATH% %JAVAPROP% org.junit.runner.JUnitCore TeamTalkStdTestSuite
+@set JAVAPROP=%JAVAPROP% -Ddk.bearware.sndinputid=%INPUTDEVICEID%
+@set JAVAPROP=%JAVAPROP% -Ddk.bearware.sndoutputid=%OUTPUTDEVICEID%
+@set JAVAPROP=%JAVAPROP% -Ddk.bearware.encrypted=%ENCRYPTED%
+@set JAVAPROP=%JAVAPROP% -Ddk.bearware.videodevid=%VIDEODEVICEID%
+@set JAVAPROP=%JAVAPROP% -Ddk.bearware.serverip=%SERVERIP%
+@set JAVAPROP=%JAVAPROP% -Ddk.bearware.serverbindip=%SERVERBINDIP%
+@set JAVAPROP=%JAVAPROP% -Ddk.bearware.videofile=%VIDEOFILE%
+@set JAVAPROP=%JAVAPROP% -Ddk.bearware.audiofile=%AUDIOFILE%
+@set JAVAPROP=%JAVAPROP% -Ddk.bearware.httpsfile=%HTTPSFILE%
+@set JAVAPROP=%JAVAPROP% -Ddk.bearware.opustools=%OPUSTOOLS%
+@set JAVAPROP=%JAVAPROP% -Ddk.bearware.verbose=%VERBOSE%
+@set JAVAPROP=%JAVAPROP% -Ddk.bearware.speexdsp=%SPEEXDSP%
+@set JAVAPROP=%JAVAPROP% -Ddk.bearware.webrtc=%WEBRTC%
+@set JAVAPROP=%JAVAPROP% -Ddk.bearware.githubskip=%GITHUBSKIP%
+@set JAVAPROP=%JAVAPROP% -Ddk.bearware.debug=%GITHUBDEBUG%
 
 @REM :again
 @REM java.exe -cp %CLASSPATH% %JAVAPROP% SingleJUnitTestRunner dk.bearware.TeamTalkStdTestCase#testVirtualSoundDevice
 @REM if %ERRORLEVEL% EQU 0 goto again
 
-cd %TEAMTALKJNI_HOME%
+REM %1 can be TeamTalkStdTestSuite TeamTalkProTestSuite TeamTalkServerTestSuite
+if %1 EQU TeamTalkStdTestSuite set CLASSPATH=%CLASSPATH%;%TEAMTALKJNI_HOME%\libs\TeamTalk5.jar;%TEAMTALKJNI_HOME%\libs\TeamTalk5Test.jar
+if %1 EQU TeamTalkProTestSuite set CLASSPATH=%CLASSPATH%;%TEAMTALKJNI_HOME%\libs\TeamTalk5Pro.jar;%TEAMTALKJNI_HOME%\libs\TeamTalk5ProTest.jar
+if %1 EQU TeamTalkServerTestSuite set CLASSPATH=%CLASSPATH%;%TEAMTALKJNI_HOME%\libs\TeamTalk5Pro.jar;%TEAMTALKJNI_HOME%\libs\TeamTalk5SrvTest.jar
+
+cd test
+java.exe -cp %CLASSPATH% %JAVAPROP% org.junit.runner.JUnitCore %1
