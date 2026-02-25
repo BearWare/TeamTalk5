8f27a15c2a948a73e8d868c0c46a356f1aba1bf0
Bjørn Damstedt Rasmussen <contact@bearware.dk>
2026-02-25 13:59:00 +0100

    License check in GitHub artifacts

---
+++ Library/TeamTalkLib/license/Trial.h
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2005-2018, BearWare.dk
+ * 
+ * Contact Information:
+ *
+ * Bjoern D. Rasmussen
+ * Kirketoften 5
+ * DK-8260 Viby J
+ * Denmark
+ * Email: contact@bearware.dk
+ * Phone: +45 20 20 54 59
+ * Web: http://www.bearware.dk
+ *
+ * This source code is part of the TeamTalk SDK owned by
+ * BearWare.dk. Use of this file, or its compiled unit, requires a
+ * TeamTalk SDK License Key issued by BearWare.dk.
+ *
+ * The TeamTalk SDK License Agreement along with its Terms and
+ * Conditions are outlined in the file License.txt included with the
+ * TeamTalk SDK distribution.
+ *
+ */
+
+#if !defined(TRIAL_H)
+#define TRIAL_H
+
+#include "myace/MyACE.h"
+
+#define REG_NAME_MIN 5
+
+void LicenseCheck();
+
+void ValidTeamTalkSDK();
+
+ACE_TString GetProcessName();
+
+#endif
