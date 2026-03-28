732fa010fa07a37eab43d81c118a2c1937a73b8d
Sihu Hwang <129564966+hwangsihu@users.noreply.github.com>
2026-03-28 17:00:03 +0900

    Add UPnP renew and refactoring (#3171)
    * Add UPnP renew
    
    Fixes an issue where the server would need to be restarted when the router was restarted or the network connection changed
    
    * Use existing variables to query state of UPnP
    
    Refactored UPnP state variables.
    
    ---------
    
    Co-authored-by: Bjørn Damstedt Rasmussen <contact@bearware.dk>

--- Library/TeamTalkLib/bin/ttsrv/UPnP.cpp
+++ Library/TeamTalkLib/bin/ttsrv/UPnP.cpp
@@ -28,12 +28,13 @@
 #include <miniupnpc/upnpcommands.h>
 #include <miniupnpc/upnperrors.h>
 
+#include <array>
 #include <cstring>
 #include <string>
 
 static struct UPNPUrls s_urls = {};
 static struct IGDdatas s_data = {};
-static bool s_initialized = false;
+std::string lanIPAddr;
 
 bool UPnP_AddPortMapping(uint16_t tcpport, uint16_t udpport, std::string& externalIP)
 {
@@ -45,52 +46,78 @@ bool UPnP_AddPortMapping(uint16_t tcpport, uint16_t udpport, std::string& extern
         return false;
     }
 
-    char lanaddr[40] = {};
+    std::array<char,40> s_lanaddr;
 #if MINIUPNPC_API_VERSION >= 18
-    char wanaddr[40] = {};
+    std::array<char, 40> wanaddr;
     int r = UPNP_GetValidIGD(devlist, &s_urls, &s_data,
-                              lanaddr, sizeof(lanaddr),
-                              wanaddr, sizeof(wanaddr));
+                              s_lanaddr.data(), s_lanaddr.size(),
+                              wanaddr.data(), wanaddr.size());
 #else
     int r = UPNP_GetValidIGD(devlist, &s_urls, &s_data,
-                              lanaddr, sizeof(lanaddr));
+                             s_lanaddr.data(), s_lanaddr.size());
 #endif
     freeUPNPDevlist(devlist);
 
-    if (r != 1 && r != 2)
+    switch (r)
     {
+    case UPNP_CONNECTED_IGD :
+    case UPNP_PRIVATEIP_IGD :
+        break;
+    default :
         TT_LOG(ACE_TEXT("UPnP: No valid IGD found."));
         return false;
     }
 
-    char extIP[16] = {};
-    UPNP_GetExternalIPAddress(s_urls.controlURL,
-                              s_data.first.servicetype, extIP);
-    externalIP = extIP;
+    std::array<char, 16> extIP;
+    switch (UPNP_GetExternalIPAddress(s_urls.controlURL,
+                                      s_data.first.servicetype, extIP.data()))
+    {
+    case UPNPCOMMAND_SUCCESS:
+        externalIP = extIP.data();
+        break;
+    default :
+        TT_LOG(ACE_TEXT("UPnP: Failed to get the external IP-address."));
+        return false;
+    }
+
+    lanIPAddr = s_lanaddr.data();
+
+    return UPnP_RenewPortMapping(tcpport, udpport);
+}
+
+bool UPnP_RenewPortMapping(uint16_t tcpport, uint16_t udpport)
+{
+    if (lanIPAddr.empty())
+        return false;
 
     std::string const tcp_str = std::to_string(tcpport);
     int ret = UPNP_AddPortMapping(s_urls.controlURL,
         s_data.first.servicetype,
-        tcp_str.c_str(), tcp_str.c_str(), lanaddr,
+        tcp_str.c_str(), tcp_str.c_str(), lanIPAddr.c_str(),
         "TeamTalk Server", "TCP", "", "0");
     if (ret != UPNPCOMMAND_SUCCESS)
+    {
         TT_LOG(ACE_TEXT("UPnP: Failed to add TCP port mapping."));
+        return false;
+    }
 
     std::string const udp_str = std::to_string(udpport);
     ret = UPNP_AddPortMapping(s_urls.controlURL,
         s_data.first.servicetype,
-        udp_str.c_str(), udp_str.c_str(), lanaddr,
+        udp_str.c_str(), udp_str.c_str(), lanIPAddr.c_str(),
         "TeamTalk Server", "UDP", "", "0");
     if (ret != UPNPCOMMAND_SUCCESS)
+    {
         TT_LOG(ACE_TEXT("UPnP: Failed to add UDP port mapping."));
+        return false;
+    }
 
-    s_initialized = true;
     return true;
 }
 
 void UPnP_RemovePortMapping(uint16_t tcpport, uint16_t udpport)
 {
-    if (!s_initialized)
+    if (lanIPAddr.empty())
         return;
 
     std::string const tcp_str = std::to_string(tcpport);
@@ -102,8 +129,7 @@ void UPnP_RemovePortMapping(uint16_t tcpport, uint16_t udpport)
         s_data.first.servicetype, udp_str.c_str(), "UDP", "");
 
     FreeUPNPUrls(&s_urls);
-    std::memset(&s_data, 0, sizeof(s_data));
-    s_initialized = false;
+    lanIPAddr.clear();
 
     TT_LOG(ACE_TEXT("UPnP: Port mappings removed."));
 }
