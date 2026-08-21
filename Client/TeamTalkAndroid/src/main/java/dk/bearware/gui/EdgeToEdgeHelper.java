9d4d8defb55aafc5612c791a81804388306db8ee
Amir <amirmahdifard0@gmail.com>
2026-08-21 11:30:29 +0330

    Fix the screen reader focus bug in app action bars on android 15 and above

--- Client/TeamTalkAndroid/src/main/java/dk/bearware/gui/EdgeToEdgeHelper.java
+++ Client/TeamTalkAndroid/src/main/java/dk/bearware/gui/EdgeToEdgeHelper.java
@@ -27,18 +27,16 @@ import android.app.Activity;
 import android.os.Build;
 import android.view.View;
 
+import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class EdgeToEdgeHelper {
-
     public static void enableEdgeToEdge(Activity activity) {
-        View rootView = activity.findViewById(android.R.id.content);
+        View rootView = (View) activity.findViewById(android.R.id.content).getParent();
         ViewCompat.setOnApplyWindowInsetsListener(rootView, (v, insets) -> {
-            int topInset = insets.getInsets(WindowInsetsCompat.Type.statusBars()).top;
-            int bottomInset = insets.getInsets(WindowInsetsCompat.Type.navigationBars()).bottom;
-
-            v.setPadding(v.getPaddingLeft(), topInset, v.getPaddingRight(), bottomInset);
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
     }
