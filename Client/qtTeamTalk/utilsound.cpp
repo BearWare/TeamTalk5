f890aa8f88b1c1b4b45763f4331930a968733eb8
Quin Gillespie <trypsynth@gmail.com>
2026-09-20 17:02:03 -0600

    Qt client: Fall back to the default sound device instead of a stale device index

--- Client/qtTeamTalk/utilsound.cpp
+++ Client/qtTeamTalk/utilsound.cpp
@@ -208,9 +208,11 @@ int getSoundInputFromUID(int inputid, const QString& uid)
 
     SoundDevice dev;
     if (getSoundDevice(uid, true, inputdev, dev))
-        inputid = dev.nDeviceID;
+        return dev.nDeviceID;
 
-    return inputid;
+    // the selected device is gone and its ID now belongs to another device,
+    // so fall back to the default rather than opening whatever took its place
+    return getDefaultSndInputDevice();
 }
 
 int getSoundOutputFromUID(int outputid, const QString& uid)
@@ -222,8 +224,9 @@ int getSoundOutputFromUID(int outputid, const QString& uid)
 
     SoundDevice dev;
     if (getSoundDevice(uid, false, outputdev, dev))
-        outputid = dev.nDeviceID;
-    return outputid;
+        return dev.nDeviceID;
+
+    return getDefaultSndOutputDevice();
 }
 
 int getSelectedSndInputDevice()
